eea76f885f96a70de3558132b0ad5f30d8dfaf9c
Invertisment <martynas.maciulevicius@pm.me>
2020-06-30 17:04:16 +0300

    cleanup

--- packages/builder/src/builtin-tasks/test.ts
+++ packages/builder/src/builtin-tasks/test.ts
@@ -15,11 +15,7 @@ export async function glob(pattern: string): Promise<string[]> {
 }
 
 import {
-  //TASK_COMPILE,
-  //TASK_TEST,
   TASK_TEST_GET_TEST_FILES,
-  //TASK_TEST_RUN_MOCHA_TESTS,
-  //TASK_TEST_SETUP_TEST_ENVIRONMENT,
 } from "./task-names";
 
 export default function () {
@@ -48,72 +44,4 @@ export default function () {
       return [...jsFiles, ...tsFiles];
     });
 
-  //internalTask(TASK_TEST_SETUP_TEST_ENVIRONMENT, async () => {});
-
-  //internalTask(TASK_TEST_RUN_MOCHA_TESTS)
-  //  .addOptionalVariadicPositionalParam(
-  //    "testFiles",
-  //    "An optional list of files to test",
-  //    []
-  //  )
-  //  .setAction(async ({ testFiles }: { testFiles: string[] }, { config }) => {
-  //    const { default: Mocha } = await import("mocha");
-  //    const mocha = new Mocha(config.mocha);
-  //    testFiles.forEach((file) => mocha.addFile(file));
-
-  //    const runPromise = new Promise<number>((resolve, _) => {
-  //      mocha.run(resolve);
-  //    });
-
-  //    process.exitCode = await runPromise;
-  //  });
-
-  //task(TASK_TEST, "Runs mocha tests")
-  //  .addOptionalVariadicPositionalParam(
-  //    "testFiles",
-  //    "An optional list of files to test",
-  //    []
-  //  )
-  //  .addFlag("noCompile", "Don't compile before running this task")
-  //  .setAction(
-  //    async (
-  //      {
-  //        testFiles,
-  //        noCompile,
-  //      }: {
-  //        testFiles: string[];
-  //        noCompile: boolean;
-  //      },
-  //      { run, network }
-  //    ) => {
-  //      if (!noCompile) {
-  //        await run(TASK_COMPILE);
-  //      }
-
-  //      const files = await run(TASK_TEST_GET_TEST_FILES, { testFiles });
-  //      await run(TASK_TEST_SETUP_TEST_ENVIRONMENT);
-  //      await run(TASK_TEST_RUN_MOCHA_TESTS, { testFiles: files });
-
-  //      if (network.name !== BUIDLEREVM_NETWORK_NAME) {
-  //        return;
-  //      }
-
-  //      const failures = await network.provider.send(
-  //        "buidler_getStackTraceFailuresCount"
-  //      );
-
-  //      if (failures === 0) {
-  //        return;
-  //      }
-
-  //      console.warn(
-  //        chalk.yellow(
-  //          `Failed to generate ${failures} ${pluralize(
-  //            failures,
-  //            "stack trace"
-  //          )}. Run Buidler with --verbose to learn more.`
-  //        )
-  //      );
-  //    }
-  //  );
 }
