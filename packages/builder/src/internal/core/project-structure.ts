2261f3fd993821a8759b101a8d3d1a4fc61ac055
Robert Zaremba <robert@zaremba.ch>
2020-07-06 11:22:36 +0200

    remove TS config file check with thrown exception

--- packages/builder/src/internal/core/project-structure.ts
+++ packages/builder/src/internal/core/project-structure.ts
@@ -1,16 +1,12 @@
 import findUp from "find-up";
 
 export const JS_CONFIG_FILENAME = "algob.config.js";
-export const TS_CONFIG_FILENAME = "algob.config.ts";
+// export const TS_CONFIG_FILENAME = "algob.config.ts";
 
 export function isCwdInsideProject() : boolean {
   return Boolean(findUp.sync(JS_CONFIG_FILENAME));
 }
 
 export function getUserConfigPath(): string | undefined {
-  if (findUp.sync(TS_CONFIG_FILENAME)) {
-    throw "TypeScript config was found but TypeScript is not supported."
-  }
-
   return findUp.sync(JS_CONFIG_FILENAME);
 }
