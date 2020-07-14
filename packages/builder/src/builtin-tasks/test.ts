1f1b781eb553f9d95331e7e1e8f47608372c1a0b
Robert Zaremba <robert@zaremba.ch>
2020-07-14 16:36:07 +0200

    cleaning: removing util module from builtin-tasks
    https://www.pivotaltracker.com/n/projects/2452320#

--- packages/builder/src/builtin-tasks/test.ts
+++ packages/builder/src/builtin-tasks/test.ts
@@ -1,29 +1,35 @@
+import glob from "glob";
 import path from "path";
 
 import { internalTask } from "../internal/core/config/config-env";
-import { AlgobRuntimeEnv } from "../types";
+import type { AlgobRuntimeEnv, ResolvedAlgobConfig } from "../types";
+import { TaskArgs } from "./deploy";
 import {
   TASK_TEST_GET_TEST_FILES,
 } from "./task-names";
-import { glob } from "./util";
+
 
 export default function () : void {
   internalTask(TASK_TEST_GET_TEST_FILES)
     .addOptionalVariadicPositionalParam(
-      "testFiles",
+      "filenames",
       "An optional list of files to test",
       []
     )
     .setAction(runTests);
 }
 
-async function runTests({ testFiles }: { testFiles: string[] }, { config }: AlgobRuntimeEnv) {
-  if (testFiles.length !== 0) {
-    return testFiles;
+function loadFiles(fileNames: string[], config: ResolvedAlgobConfig): string[]{
+  if (fileNames.length !== 0) {
+    return fileNames;
   }
   if (!config.paths) {
     throw new Error("unexpected non-project execution")
   }
+  return glob.sync(path.join(config.paths.tests, "**/*.js")).sort();
+}
 
-  return glob(path.join(config.paths.tests, "**/*.js"));
+async function runTests({ fileNames }: TaskArgs, { config }: AlgobRuntimeEnv): Promise<void> {
+  const testfiles = loadFiles;
+  console.log("Test files:", testfiles);
 }
