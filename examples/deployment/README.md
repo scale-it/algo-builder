d20766926f7278cae363cbaa468f1f8dad967922
Invertisment <martynas.maciulevicius@pm.me>
2020-08-31 11:13:21 +0300

    Config and README.md update

--- examples/deployment/README.md
+++ examples/deployment/README.md
@@ -1,6 +1,33 @@
-ASA deployment using Algorand builder from Scale it
+# ASA deployment using [Algorand Builder](https://github.com/scale-it/algorand-builder/)
 
-Needs to be linked (`yarn link`) with algob:
-https://github.com/scale-it/algorand-builder/
+## Usage
+### Create your local network:
+https://developer.algorand.org/tutorials/create-private-network/
 
-Authors: Invertisment & amityadav0
+### Start whole network:
+`goal network start -r ~/.algorand-local/`
+
+### Start/stop a single node:
+`goal node start -d ~/.algorand-local/Node/`
+
+### Currently it needs to be linked with algob:
+`git clone https://github.com/scale-it/algorand-builder/`
+`cd algorand-builder/packages/algob`
+`yarn`
+`yarn link`
+
+### Change your keys
+Use your editor to edit `algob.config.js`
+
+### Run
+`algob deploy`
+`algob run scripts/query/john-balances.js`
+
+## Authors
+- robert-zaremba
+- Invertisment
+- amityadav0
+- as04
+
+### Repository of this example (may be updated less often):
+https://github.com/Invertisment/algob-asa-deploy-opt-in
--- examples/deployment/algob.config.js
+++ examples/deployment/algob.config.js
@@ -17,15 +17,22 @@
 //}]
 
 const { mkAccounts } = require("algob");
-let accounts = mkAccounts([{
-  name: "master-account",
-  // goal account list -d ~/.algorand-local/Node/
-  addr: "WWYNX3TKQYVEREVSW6QQP3SXSFOCE3SKUSEIVJ7YAGUPEACNI5UGI4DZCE",
-  // Replace this with your own account.
-  // Export using:
-  // goal account export -a WWYNX3TKQYVEREVSW6QQP3SXSFOCE3SKUSEIVJ7YAGUPEACNI5UGI4DZCE -d ~/.algorand-local/Node/
-  mnemonic: "enforce drive foster uniform cradle tired win arrow wasp melt cattle chronic sport dinosaur announce shell correct shed amused dismiss mother jazz task above hospital"
-},
+let accounts = mkAccounts([
+  {
+    name: "master-account",
+    // This account is exported from algorand node.
+    // It must already exist or ALGO have to be transferred from elsewhere.
+    // It contains a lot of ALGO tokens so it can fund other accounts.
+    // You should view your accounts and replace this with your own account.
+    // Command to view account addresses (and their balances):
+    // goal account list -d ~/.algorand-local/Node/
+    addr: "WWYNX3TKQYVEREVSW6QQP3SXSFOCE3SKUSEIVJ7YAGUPEACNI5UGI4DZCE",
+    // To export private mnemonic you must use this command (with you own account's address and data directory)
+    // goal account export -a WWYNX3TKQYVEREVSW6QQP3SXSFOCE3SKUSEIVJ7YAGUPEACNI5UGI4DZCE -d ~/.algorand-local/Node/
+    mnemonic: "enforce drive foster uniform cradle tired win arrow wasp melt cattle chronic sport dinosaur announce shell correct shed amused dismiss mother jazz task above hospital"
+  },
+  // Following accounts are generated using `algob gen-accounts`.
+  // Do not use them in your own applications.
   {
     name: "gold-owner-account",
     addr: "M7VR2MGHI35EG2NMYOF3X337636PIOFVSP2HNIFUKAG7WW6BDWDCA3E2DA",
@@ -47,12 +54,6 @@ let accounts = mkAccounts([{
 // // Example: accounts loaded from a file:
 // const accFromFile = loadAccountsFromFileSync("assets/accounts_generated.yaml");
 // accounts = accounts.concat(accFromFile);
-
-// Node commands:
-// Start whole network:
-// goal network start -r ~/.algorand-local/
-// Start/stop a single node:
-// goal node start -d ~/.algorand-local/Node/
 
 let defaultCfg = {
   host: "http://localhost",
