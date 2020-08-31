84402909906c43dc66de7b66a9cda5584e4ef695
Invertisment <martynas.maciulevicius@pm.me>
2020-08-31 12:00:18 +0300

    Config notice update

--- examples/deployment/algob.config.js
+++ examples/deployment/algob.config.js
@@ -1,6 +1,7 @@
-// NOTE: below we provide some example accounts.
-// DON'T this account in any working environment because everyone can check it and use
-// the private keys (this accounts are visible to everyone).
+// NOTICE: This config provides sample accounts.
+// DON'T use these accounts in any public environment because everyone can see and use them.
+// The private keys of these accounts are visible to everyone.
+// This means that they can spend the funds and assets.
 
 // Example: accounts constructed using mnemonic. `addr` is optional.
 // const { mkAccounts } = require("algob");
@@ -21,18 +22,19 @@ let accounts = mkAccounts([
   {
     name: "master-account",
     // This account is exported from algorand node.
-    // It must already exist or ALGO have to be transferred from elsewhere.
+    // It must already exist or ALGO tokens have to be transferred from elsewhere.
     // It contains a lot of ALGO tokens so it can fund other accounts.
     // You should view your accounts and replace this with your own account.
     // Command to view account addresses (and their balances):
     // goal account list -d ~/.algorand-local/Node/
     addr: "WWYNX3TKQYVEREVSW6QQP3SXSFOCE3SKUSEIVJ7YAGUPEACNI5UGI4DZCE",
-    // To export private mnemonic you must use this command (with you own account's address and data directory)
+    // To export private mnemonic you may use this command (with you own account's address and data directory):
     // goal account export -a WWYNX3TKQYVEREVSW6QQP3SXSFOCE3SKUSEIVJ7YAGUPEACNI5UGI4DZCE -d ~/.algorand-local/Node/
     mnemonic: "enforce drive foster uniform cradle tired win arrow wasp melt cattle chronic sport dinosaur announce shell correct shed amused dismiss mother jazz task above hospital"
   },
   // Following accounts are generated using `algob gen-accounts`.
-  // Do not use them in your own applications.
+  // Do not use these exact accounts in your own applications.
+  // See notice at the top of this file.
   {
     name: "gold-owner-account",
     addr: "M7VR2MGHI35EG2NMYOF3X337636PIOFVSP2HNIFUKAG7WW6BDWDCA3E2DA",
