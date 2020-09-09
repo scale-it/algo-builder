93d96c32031a2a59b1e7eef0d29b4d93e202d186
Robert Zaremba <robert@zaremba.ch>
2020-09-09 16:53:53 +0200

    demo: update gold SC funding

--- examples/deployment/scripts/2-gold-asc.js
+++ examples/deployment/scripts/2-gold-asc.js
@@ -11,7 +11,7 @@ async function run(runtimeEnv, deployer) {
   const goldOwnerAccount = deployer.accountsByName.get("gold-owner-account");
 
   const ascInfo = await deployer.deployASC("3-gold-asc.teal", [],
-  {funder: goldOwnerAccount, fundingMicroAlgo: 1000000 }, {}); // sending 1 Algo
+    {funder: goldOwnerAccount, fundingMicroAlgo: 101000 }, {}); // sending 0.101 Algo
 
   console.log(ascInfo);
 }
