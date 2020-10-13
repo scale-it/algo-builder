7a4125f938e030aad76d7fb5fa49c7035b7d4358
ratik <ratikjindal21@gmail.com>
2020-10-14 03:39:29 +0530

    add common function to handle errors

--- examples/deployment/scripts/transfer/gold_contract_sc.js
+++ examples/deployment/scripts/transfer/gold_contract_sc.js
@@ -1,4 +1,9 @@
-const { transferMicroAlgosLsig } = require("algob");
+/**
+ * Description:
+ * This file demonstrates the example to transfer MicroAlgos
+ * from contract account (contract approval mode) to another according to smart contract (ASC) logic
+*/
+const { transferAlgo } = require('./common');
 
 async function run(runtimeEnv, deployer) {
   const johnAccount = deployer.accountsByName.get("john-account");
@@ -6,29 +11,17 @@ async function run(runtimeEnv, deployer) {
 
   // Transactions for Transaction for ALGO - Contract : '2-gold-contract-asc.teal'  (Contract Approval Mode)
   const lsig = await deployer.getLogicSignature("2-gold-contract-asc.teal", []);
-  
-  const sender = lsig.address(); // sender is contract account
+  // sender is contract account
+  const sender = lsig.address(); 
 
-  // Will pass - As according to .teal logic, amount should be >= 100 and receiver should be john
-  const details = await transferMicroAlgosLsig(deployer, sender, johnAccount.addr, 200, lsig);
-  console.log(details);
+  // Transaction PASS - As according to .teal logic, amount should be <= 100 and receiver should be john
+  await transferAlgo(deployer, { addr: sender}, johnAccount.addr, 20, lsig);
   
-  // Gets rejected by logic - As according to .teal logic, amount should be >= 100
-  try {
-    await transferMicroAlgosLsig(deployer, sender, johnAccount.addr, 20, lsig);
-  } catch (e) {
-    console.log('Transaction Failed - rejected by logic');
-    //console.error(e);
-  }
+  // Transaction FAIL - Gets rejected by logic - As according to .teal logic, amount should be <= 100
+  await transferAlgo(deployer, { addr: sender}, johnAccount.addr, 200, lsig);
 
-  // Transaction fail as Elon tried to receive instead of John
-  try {
-    await transferMicroAlgosLsig(deployer, sender, elonMuskAccount.addr, 500, lsig);
-  } catch(e) {
-    console.log('Transaction Failed - rejected by logic');
-    //console.error(e);
-  }
-  
+  // Transaction FAIL as Elon tried to receive instead of John
+  await transferAlgo(deployer, { addr: sender}, elonMuskAccount.addr, 50, lsig);
 }
 
 module.exports = { default: run }
