4d5536f1d80d9790e8fc15dfff76168761f28fac
ratik <ratikjindal21@gmail.com>
2020-10-14 15:41:40 +0530

    rename StatelessASCMode to ASC1Mode

--- examples/deployment/scripts/2-gold-asc.js
+++ examples/deployment/scripts/2-gold-asc.js
@@ -1,4 +1,4 @@
-const { transferMicroAlgos, transferAsset, balanceOf, StatelessASCMode } = require("algob");
+const { transferMicroAlgos, transferAsset, balanceOf, ASC1Mode } = require("algob");
 
 async function run(runtimeEnv, deployer) {
 
@@ -8,13 +8,13 @@ async function run(runtimeEnv, deployer) {
   await transferMicroAlgos(deployer, masterAccount, goldOwnerAccount.addr, 200000000, {note: "funding account"});
 
   const ascInfoGold = await deployer.deployASC("4-gold-asa.teal", [],
-    { funder: goldOwnerAccount, fundingMicroAlgo: 101000, mode: StatelessASCMode.DELEGATED_APPROVAL }, {}); // sending 0.101 Algo
+    { funder: goldOwnerAccount, fundingMicroAlgo: 101000, mode: ASC1Mode.DELEGATED_APPROVAL }, {}); // sending 0.101 Algo
 
   const ascInfoAlgoDelegated = await deployer.deployASC("3-gold-delegated-asc.teal", [],
-    { funder: goldOwnerAccount, fundingMicroAlgo: 101000, mode: StatelessASCMode.DELEGATED_APPROVAL }, {}); // sending 0.101 Algo
+    { funder: goldOwnerAccount, fundingMicroAlgo: 101000, mode: ASC1Mode.DELEGATED_APPROVAL }, {}); // sending 0.101 Algo
 
   const ascInfoAlgoContract = await deployer.deployASC("2-gold-contract-asc.teal", [],
-    { funder: goldOwnerAccount, fundingMicroAlgo: 101000, mode: StatelessASCMode.DELEGATED_APPROVAL }, {});   // sending 0.101 Algo
+    { funder: goldOwnerAccount, fundingMicroAlgo: 101000, mode: ASC1Mode.DELEGATED_APPROVAL }, {});   // sending 0.101 Algo
 
   console.log(ascInfoGold);
   console.log(ascInfoAlgoDelegated);
