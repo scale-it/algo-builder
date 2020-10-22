f828e5cfc46c307b78c7076519ea421e0e29a56d
Amit Yadav <amy29981@gmail.com>
2020-10-22 09:57:56 +0530

    object

--- packages/algob/src/lib/pyteal.ts
+++ packages/algob/src/lib/pyteal.ts
@@ -1,4 +1,4 @@
-import { spawnSync, SpawnSyncReturns } from "child_process"
+import { spawnSync, SpawnSyncReturns } from "child_process";
 import path from "path";
 
 import { ASSETS_DIR } from "../internal/core/project-structure";
@@ -9,29 +9,28 @@ const pythonExt = ".py";
  * Description: Runs a subprocess to execute python script
  * @param filename : python filename in assets folder
  */
-function runPythonScript(filename: string): SpawnSyncReturns<string> {
-    // used spawnSync instead of spawn, as it synchronous
-     return spawnSync('python3', [
-          path.join(ASSETS_DIR, filename)],
-          { encoding : 'utf8' }
-    );
+function runPythonScript (filename: string): SpawnSyncReturns<string> {
+  // used spawnSync instead of spawn, as it synchronous
+  return spawnSync('python3', [
+    path.join(ASSETS_DIR, filename)],
+  { encoding: 'utf8' }
+  );
 }
 
 /**
  * Description: returns TEAL code from pyTeal compiler
  * @param filename : python filename in assets folder
  */
-export function compilePyTeal(filename: string): string {
+export function compilePyTeal (filename: string): string {
+  if (!filename.endsWith(pythonExt)) {
+    throw new Error(`filename "${filename}" must end with "${pythonExt}"`);
+  }
+  const subprocess: SpawnSyncReturns<string> = runPythonScript(filename);
 
-    if (!filename.endsWith(pythonExt)) {
-        throw new Error(`filename "${filename}" must end with "${pythonExt}"`);
-    }
-    const subprocess: SpawnSyncReturns<string> = runPythonScript(filename);
-
-    if(subprocess.stderr) {
-        console.error(subprocess.stderr);
-        throw subprocess.stderr;
-    }
-    //output of script
-    return subprocess.stdout;
+  if (subprocess.stderr) {
+    console.error(subprocess.stderr);
+    throw Error(subprocess.stderr);
+  }
+  // output of script
+  return subprocess.stdout;
 }
