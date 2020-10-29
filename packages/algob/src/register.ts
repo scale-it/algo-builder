d107a4b4bc5965026118a2eb82023cdbbdba4c8e
Robert Zaremba <robert@zaremba.ch>
2020-10-29 12:46:05 +0100

    Update algsdk typings

--- packages/algob/src/register.ts
+++ packages/algob/src/register.ts
@@ -10,7 +10,7 @@ import {
   isNodeCalledWithoutAScript
 } from "./internal/util/console";
 
-(async () => {
+async function registerEnv() {
   if (!BuilderContext.isCreated()) {
     require("source-map-support/register");
 
@@ -47,4 +47,6 @@ import {
 
     env.injectToGlobal();
   }
-})().catch((err) => console.log(err));
+}
+
+registerEnv().catch((err) => console.error(err));
