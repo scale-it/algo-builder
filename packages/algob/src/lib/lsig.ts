a65803ca0a61a5d6c82234da9b3893467e933d39
ratik <ratikjindal21@gmail.com>
2020-11-04 16:06:08 +0530

    rollback to function for dummyLsig

--- packages/algob/src/lib/lsig.ts
+++ packages/algob/src/lib/lsig.ts
@@ -1,10 +1,8 @@
 /* eslint @typescript-eslint/no-var-requires: "off" */
 import algosdk from "algosdk";
 
-import { ASCCache } from "../types";
+import { ASCCache, LogicSig } from "../types";
 import { CompileOp } from "./compile";
-
-export const DUMMY_LSIG = algosdk.makeLogicSig(new Uint8Array(56), []);
 
 /**
  * Description: this function makes logic signature from .teal file
@@ -17,4 +15,11 @@ export async function getLsig (name: string, scParams: Object, algodClient: algo
   const result: ASCCache = await compileOp.ensureCompiled(name, false);
   const program = result.toBytes;
   return algosdk.makeLogicSig(program, scParams);
+}
+
+/**
+ * Description: this function creates and returns a dummy logic signature
+ */
+export function getDummyLsig (): LogicSig {
+  return algosdk.makeLogicSig(new Uint8Array(56), []);
 }
