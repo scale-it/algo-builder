979d5e089dfa12f9b6cf7715f698c77e3af111a5
ratik <ratikjindal21@gmail.com>
2020-11-07 00:21:30 +0530

    update tests

--- packages/algob/test/internal/util/scripts-runner.ts
+++ packages/algob/test/internal/util/scripts-runner.ts
@@ -3,11 +3,10 @@ import fs from "fs";
 
 import { ERRORS } from "../../../src/internal/core/errors-list";
 import { DeployerRunMode } from "../../../src/internal/deployer";
-import { TxWriterImpl } from "../../../src/internal/tx-log-writer";
+import { DeployerConfig } from "../../../src/internal/deployer_cfg";
 import {
   runScript
 } from "../../../src/internal/util/scripts-runner";
-import { CheckpointRepoImpl } from "../../../src/lib/script-checkpoints";
 import { expectBuilderErrorAsync } from "../../helpers/errors";
 import { mkAlgobEnv } from "../../helpers/params";
 import { testFixtureOutputFile, useCleanFixtureProject } from "../../helpers/project";
@@ -16,28 +15,31 @@ import { AlgoOperatorDryRunImpl } from "../../stubs/algo-operator";
 describe("Scripts runner", function () {
   useCleanFixtureProject("project-with-scripts");
 
+  const env = mkAlgobEnv();
+  let deployerCfg: DeployerConfig;
+
+  beforeEach(function () {
+    deployerCfg = new DeployerConfig(env, new AlgoOperatorDryRunImpl());
+    deployerCfg.asaDefs = {};
+    deployerCfg.accounts = new Map();
+  });
+
   it("Should pass params to the script", async function () {
-    const env = mkAlgobEnv();
-    await runScript("./scripts/params-script.js", env, new DeployerRunMode(
-      env, new CheckpointRepoImpl(), {}, new AlgoOperatorDryRunImpl(), new Map(), new TxWriterImpl('')));
+    await runScript("./scripts/params-script.js", env, new DeployerRunMode(deployerCfg));
     const scriptOutput = fs.readFileSync(testFixtureOutputFile).toString();
     assert.equal(scriptOutput, "network1");
   });
 
   it("Should run the script to completion", async function () {
     const before = new Date();
-    const env = mkAlgobEnv();
-    await runScript("./scripts/async-script.js", env, new DeployerRunMode(
-      env, new CheckpointRepoImpl(), {}, new AlgoOperatorDryRunImpl(), new Map(), new TxWriterImpl('')));
+    await runScript("./scripts/async-script.js", env, new DeployerRunMode(deployerCfg));
     const after = new Date();
     assert.isAtLeast(after.getTime() - before.getTime(), 20);
   });
 
   it("Exception shouldn't crash the whole app", async function () {
-    const env = mkAlgobEnv();
     await expectBuilderErrorAsync(
-      async () => await runScript("./scripts/failing-script.js", env, new DeployerRunMode(
-        env, new CheckpointRepoImpl(), {}, new AlgoOperatorDryRunImpl(), new Map(), new TxWriterImpl(''))),
+      async () => await runScript("./scripts/failing-script.js", env, new DeployerRunMode(deployerCfg)),
       ERRORS.BUILTIN_TASKS.SCRIPT_EXECUTION_ERROR,
       "./scripts/failing-script.js"
     );
@@ -46,10 +48,8 @@ describe("Scripts runner", function () {
   });
 
   it("Nonexistent default method should throw an exception", async function () {
-    const env = mkAlgobEnv();
     await expectBuilderErrorAsync(
-      async () => await runScript("./scripts/no-default-method-script.js", env, new DeployerRunMode(
-        env, new CheckpointRepoImpl(), {}, new AlgoOperatorDryRunImpl(), new Map(), new TxWriterImpl(''))),
+      async () => await runScript("./scripts/no-default-method-script.js", env, new DeployerRunMode(deployerCfg)),
       ERRORS.GENERAL.NO_DEFAULT_EXPORT_IN_SCRIPT,
       "./scripts/no-default-method-script.js"
     );
@@ -58,10 +58,8 @@ describe("Scripts runner", function () {
   });
 
   it("Should wrap error of require", async function () {
-    const env = mkAlgobEnv();
     await expectBuilderErrorAsync(
-      async () => await runScript("./scripts/failing-script-load.js", env, new DeployerRunMode(
-        env, new CheckpointRepoImpl(), {}, new AlgoOperatorDryRunImpl(), new Map(), new TxWriterImpl(''))),
+      async () => await runScript("./scripts/failing-script-load.js", env, new DeployerRunMode(deployerCfg)),
       ERRORS.GENERAL.SCRIPT_LOAD_ERROR,
       "/project-with-scripts/scripts/failing-script-load.js"
     );
@@ -70,9 +68,7 @@ describe("Scripts runner", function () {
   });
 
   it("Should ignore return value", async function () {
-    const env = mkAlgobEnv();
-    const out = await runScript("./scripts/successful-script-return-status.js", env, new DeployerRunMode(
-      env, new CheckpointRepoImpl(), {}, new AlgoOperatorDryRunImpl(), new Map(), new TxWriterImpl('')));
+    const out = await runScript("./scripts/successful-script-return-status.js", env, new DeployerRunMode(deployerCfg));
     assert.equal(out, undefined);
   });
 });
