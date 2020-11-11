9e0fd95c34913434a2607d33b6bfc2cd3888e162
Amit Yadav <amy29981@gmail.com>
2020-11-11 18:05:56 +0530

    update

--- examples/ref-templates/scripts/htlc.js
+++ examples/ref-templates/scripts/htlc.js
@@ -21,26 +21,19 @@ async function run(runtimeEnv, deployer) {
   const wrongSecret = "hero wisdom red split loop element vote belt";
 
   // setup a contract account and send 1 ALGO from master
-  await deployer.fundLsig("htlc.py", [],
-    { funder: masterAccount, fundingMicroAlgo: 1000000 }, { closeRemainderTo: johnAccount.addr }); 
+  await deployer.fundLsig("htlc.py", [], { funder: masterAccount, fundingMicroAlgo: 1000000 }, { closeRemainderTo: johnAccount.addr }); 
 
   let contract = await deployer.loadLogic("htlc.py", [ wrongSecret ]);
   let contractAddress = contract.address();
   
   // Fails as wrong secret is passed
-  await transferAlgo(deployer, 
-    { addr: contractAddress}, 
-    globalZeroAddress, 0, contract, 
-    { totalFee: 1000, closeRemainderTo: johnAccount.addr});
+  await transferAlgo(deployer, { addr: contractAddress}, globalZeroAddress, 0, contract, { totalFee: 1000, closeRemainderTo: johnAccount.addr});
 
   contract = await deployer.loadLogic("htlc.py", [ secret ]);
   contractAddress = contract.address();
 
   // Passes as right secret is passed
-  await transferAlgo(deployer, 
-    { addr: contractAddress}, 
-    globalZeroAddress, 0, contract, 
-    { totalFee: 2000, closeRemainderTo: johnAccount.addr});
+  await transferAlgo(deployer, { addr: contractAddress}, globalZeroAddress, 0, contract, { totalFee: 2000, closeRemainderTo: johnAccount.addr});
   
 }
 
