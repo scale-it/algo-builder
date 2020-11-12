ad85359f771e1598306e4f909e16c264d7300130
Amit Yadav <amy29981@gmail.com>
2020-11-12 07:45:16 +0530

    Update packages/algob/src/lib/algo-operator.ts
    Co-authored-by: Robert Zaremba <robert@zaremba.ch>

--- packages/algob/src/lib/algo-operator.ts
+++ packages/algob/src/lib/algo-operator.ts
@@ -310,7 +310,6 @@ export class AlgoOperatorImpl implements AlgoOperator {
    */
   async optInToSSC (sender: Account, appId: number, payFlags: TxParams): Promise<void> {
     const params = await tx.mkSuggestedParams(this.algodClient, payFlags);
-
     const txn = algosdk.makeApplicationOptInTxn(sender, params, appId);
     const txId = txn.txID().toString();
     const signedTxn = txn.signTxn(sender.sk);
