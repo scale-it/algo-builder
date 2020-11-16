8d19e3c5dee3d6a27faa5696b911e8dceb116782
Amit Yadav <amy29981@gmail.com>
2020-11-16 18:45:29 +0530

    update multisig and htlc-pyteal

--- examples/htlc-pyteal/scripts/fund-pyteal.js
+++ examples/htlc-pyteal/scripts/fund-pyteal.js
@@ -2,15 +2,22 @@
  * Description:
  * This file demonstrates the PyTeal Example for HTLC(Hash Time Lock Contract)
 */
-const { transferMicroAlgos } = require("algob");
+const { executeTransaction, TransactionType, SignType } = require("algob");
 
 async function run(runtimeEnv, deployer) {
 
   const masterAccount = deployer.accountsByName.get("master-account")
   const bobAccount = deployer.accountsByName.get("bob-account"); // Buyer
 
-  await transferMicroAlgos(deployer, masterAccount, bobAccount.addr, 200000000, {note: "funding account"});  
-
+  let txnParams = {
+    type: TransactionType.TransferAlgo,
+    sign: SignType.SecretKey,
+    fromAccount: masterAccount,
+    toAccountAddr: bobAccount.addr,
+    amountMicroAlgos: 200000000,
+    payFlags: {note: "funding account"}
+  }
+  await executeTransaction(deployer, txnParams);
   // secret value hashed with sha256 will produce our image hash : QzYhq9JlYbn2QdOMrhyxVlNtNjeyvyJc/I8d8VAGfGc=
   const secret = "hero wisdom green split loop element vote belt";
   
