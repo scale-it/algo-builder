6dbe0998d6db9ba10177de96010a3abc525624ff
Amit Yadav <amy29981@gmail.com>
2020-11-18 13:41:06 +0530

    update clear state

--- examples/permissioned-voting/assets/permissioned-voting-clear.py
+++ examples/permissioned-voting/assets/permissioned-voting-clear.py
@@ -1,16 +1,7 @@
 from pyteal import *
 
 def clear_state_program():
-    get_vote_of_sender = App.localGetEx(Int(0), App.id(), Bytes("voted"))
-    program = Seq([
-        get_vote_of_sender,
-        If(And(Global.round() <= App.globalGet(Bytes("VoteEnd")), get_vote_of_sender.hasValue()),
-            App.globalPut(get_vote_of_sender.value(), App.globalGet(get_vote_of_sender.value()) - Int(1))
-        ),
-        Return(Int(1))
-    ])
-
-    return program
+    return Return(Int(1))
 
 if __name__ == "__main__":
     print(compileTeal(clear_state_program(), Mode.Application))
