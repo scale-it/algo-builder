616c9179afdd446b147d94d51a16f4ef230c107e
ratik <ratikjindal21@gmail.com>
2020-11-19 20:17:56 +0530

    execute func

---
+++ packages/algorand-js/src/internal/interpreter/interpreter.ts
@@ -0,0 +1,26 @@
+import type { TxParams } from "algob/src/types";
+import { assert } from "chai";
+
+import type { AppArgs, Logic } from "../types";
+import { Stack } from "./stack";
+
+/**
+ * Description: this function executes set of logic[] passed after
+ * parsing teal code
+ * @param {TxParams} txnParams : Transaction parameters for current txn
+ * @param {Logic[]} logic : set of Logic objects
+ * @param {AppArgs} args : argument array passed to the current transaction
+ * @returns {boolean} : transaction accepted/rejected based on asc logic
+ */
+export function execute (txnParams: TxParams, logic: Logic[], args: AppArgs): boolean {
+  assert(Array.isArray(args));
+  const stack = new Stack<string | bigint>();
+  for (const l of logic) {
+    l.execute(stack); // execute each teal opcode
+  }
+  if (stack.length() > 0) {
+    const top = stack.pop() as BigInt;
+    if (typeof top === 'string' || top >= BigInt(1)) { return true; } // Logic accept
+  }
+  return false; // Logic Reject
+}
