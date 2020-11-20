9c1c9d80793bbef36ab1ccbc7bf82c7f5fe7d950
ratik <ratikjindal21@gmail.com>
2020-11-20 15:39:02 +0530

    add more tests

--- packages/algorand-js/test/internal/interpreter/opcode-list.ts
+++ packages/algorand-js/test/internal/interpreter/opcode-list.ts
@@ -2,7 +2,7 @@ import { assert } from "chai";
 
 import { ERRORS } from "../../../src/internal/core/errors-list";
 import {
-  Add, Arg_0, Arg_1, Arg_2, Arg_3, Len
+  Add, Arg_0, Arg_1, Arg_2, Arg_3, Div, Len, Mul, Sub
 } from "../../../src/internal/interpreter/opcode-list";
 import { MAX_UINT64 } from "../../../src/lib/constants";
 import { Stack } from "../../../src/lib/stack";
@@ -71,6 +71,145 @@ describe("Teal Opcodes", function () {
       expectTealError(
         () => op.execute(stack),
         ERRORS.TEAL.UINT64_OVERFLOW
+      );
+    });
+  });
+
+  describe("Sub", function () {
+    const stack = new Stack<string | bigint>();
+
+    it("should return correct subtraction of two unit64", function () {
+      stack.push(BigInt(20));
+      stack.push(BigInt(30));
+      const op = new Sub();
+      op.execute(stack);
+
+      const top = stack.pop();
+      assert.equal(top, BigInt(10));
+    });
+
+    it("should throw error with Sub if stack is below min length", function () {
+      stack.push(BigInt(1000)); // stack.length() = 1
+      const op = new Sub();
+      expectTealError(
+        () => op.execute(stack),
+        ERRORS.TEAL.ASSERT_STACK_LENGTH
+      );
+    });
+
+    it("should throw error if Sub is used with strings", function () {
+      stack.push("str1");
+      stack.push("str2");
+      const op = new Sub();
+      expectTealError(
+        () => op.execute(stack),
+        ERRORS.TEAL.INVALID_OP_ARG
+      );
+    });
+
+    it("should throw underflow error with Sub if (A - B) < 0", function () {
+      stack.push(BigInt(20));
+      stack.push(BigInt(10));
+      const op = new Sub();
+      expectTealError(
+        () => op.execute(stack),
+        ERRORS.TEAL.UINT64_UNDERFLOW
+      );
+    });
+  });
+
+  describe("Mul", function () {
+    const stack = new Stack<string | bigint>();
+
+    it("should return correct multiplication of two unit64", function () {
+      stack.push(BigInt(20));
+      stack.push(BigInt(30));
+      const op = new Mul();
+      op.execute(stack);
+
+      const top = stack.pop();
+      assert.equal(top, BigInt(600));
+    });
+
+    it("should throw error with Mul if stack is below min length", function () {
+      stack.push(BigInt(1000)); // stack.length() = 1
+      const op = new Mul();
+      expectTealError(
+        () => op.execute(stack),
+        ERRORS.TEAL.ASSERT_STACK_LENGTH
+      );
+    });
+
+    it("should throw error if Mul is used with strings", function () {
+      stack.push("str1");
+      stack.push("str2");
+      const op = new Mul();
+      expectTealError(
+        () => op.execute(stack),
+        ERRORS.TEAL.INVALID_OP_ARG
+      );
+    });
+
+    it("should throw overflow error with Mul if (A * B) > max_unit64", function () {
+      stack.push(MAX_UINT64 - BigInt(5));
+      stack.push(BigInt(2));
+      const op = new Mul();
+      expectTealError(
+        () => op.execute(stack),
+        ERRORS.TEAL.UINT64_OVERFLOW
+      );
+    });
+  });
+
+  describe("Div", function () {
+    const stack = new Stack<string | bigint>();
+
+    it("should return correct division of two unit64", function () {
+      stack.push(BigInt(20));
+      stack.push(BigInt(40));
+      const op = new Div();
+      op.execute(stack);
+
+      const top = stack.pop();
+      assert.equal(top, BigInt(2));
+    });
+
+    it("should return 0 on division of two unit64 with A == 0", function () {
+      stack.push(BigInt(40));
+      stack.push(BigInt(0));
+      const op = new Div();
+      op.execute(stack);
+
+      const top = stack.pop();
+      assert.equal(top, BigInt(0));
+    });
+
+    it("should throw error with Div if stack is below min length", function () {
+      stack.push(BigInt(1000)); // stack.length() = 1
+      const op = new Div();
+      expectTealError(
+        () => op.execute(stack),
+        ERRORS.TEAL.ASSERT_STACK_LENGTH
+      );
+    });
+
+    it("should throw error if Div is used with strings", function () {
+      stack.push("str1");
+      stack.push("str2");
+      const op = new Div();
+      expectTealError(
+        () => op.execute(stack),
+        ERRORS.TEAL.INVALID_OP_ARG
+      );
+    });
+
+    it("should panic on A/B if B == 0", function () {
+      stack.push(BigInt(0));
+      stack.push(BigInt(10));
+      const op = new Div();
+      expectTealError(
+        () => op.execute(stack),
+        ERRORS.TEAL.INVALID_OP_ARG
       );
     });
   });
