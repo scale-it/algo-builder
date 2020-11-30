baa8c618bae67996ce41510fb9b35e6547426eba
ratik <ratikjindal21@gmail.com>
2020-11-30 18:02:08 +0530

    update example

--- examples/multisig/scripts/common/common.js
+++ examples/multisig/scripts/common/common.js
@@ -4,6 +4,6 @@ exports.executeTransaction = async function (deployer, txnParams){
     try {
        await executeTransaction(deployer, txnParams);
     } catch (e) {
-        console.error('Transaction Failed', e.response.error);
+        console.error('Transaction Failed', e.response ? e.response.error : e);
     }
 }
