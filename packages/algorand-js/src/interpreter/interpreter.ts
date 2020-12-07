def37452c3ebb679f560e67df18bd793296d8495
ratik <ratikjindal21@gmail.com>
2020-12-07 18:07:30 +0530

    add transaction context

--- packages/algorand-js/src/interpreter/interpreter.ts
+++ packages/algorand-js/src/interpreter/interpreter.ts
@@ -1,6 +1,9 @@
+import { mkTransaction } from "algob";
 import type { execParams } from "algob/src/types";
+import { assignGroupID, Transaction } from "algosdk";
 import { assert } from "chai";
 
+import { mockSuggestedParams } from "../../test/mocks/txn";
 import { TealError } from "../errors/errors";
 import { ERRORS } from "../errors/errors-list";
 import { DEFAULT_STACK_ELEM } from "../lib/constants";
@@ -12,24 +15,53 @@ export class Interpreter {
   bytecblock: Uint8Array[];
   intcblock: BigInt[];
   scratch: StackElem[];
+  txnContext: Transaction | Transaction[];
 
   constructor () {
     this.stack = new Stack<StackElem>();
     this.bytecblock = [];
     this.intcblock = [];
     this.scratch = new Array(256).fill(DEFAULT_STACK_ELEM);
+    this.txnContext = [];
   }
 
   /**
- * Description: this function executes set of Operator[] passed after
- * parsing teal code
- * @param {execParams} txn : Transaction parameters for current txn
- * @param {Logic[]} logic : set of Logic objects
- * @param {AppArgs} args : argument array passed to the current transaction
- * @returns {boolean} : transaction accepted/rejected based on asc logic
- */
-  execute (txn: execParams, logic: Operator[], args: Uint8Array[]): boolean {
+   * Description: creates a new transaction object from given execParams
+   * @param  txnParams : Transaction parameters for current txn or txn Group
+   * @returns Transaction object
+   */
+  createTxnContext (txnParams: execParams | execParams[]): Transaction | Transaction[] {
+    if (Array.isArray(txnParams)) {
+      if (txnParams.length > 16) {
+        throw new Error("Maximum size of an atomic transfer group is 16");
+      }
+
+      const txns = [];
+      for (const txnParam of txnParams) {
+        const mockParams = mockSuggestedParams(txnParam.payFlags);
+        const txn = mkTransaction(txnParam, mockParams);
+        txns.push(txn);
+      }
+      assignGroupID(txns);
+      return txns;
+    } else {
+      const mockParams = mockSuggestedParams(txnParams.payFlags);
+      return mkTransaction(txnParams, mockParams);
+    }
+  }
+
+  /**
+   * Description: this function executes set of Operator[] passed after
+   * parsing teal code
+   * @param {execParams} txn : Transaction parameters for current txn
+   * @param {Logic[]} logic : set of Logic objects
+   * @param {AppArgs} args : argument array passed to the current transaction
+   * @returns {boolean} : transaction accepted/rejected based on asc logic
+   */
+  execute (txnParams: execParams | execParams[], logic: Operator[], args: Uint8Array[]): boolean {
     assert(Array.isArray(args));
+    this.txnContext = this.createTxnContext(txnParams);
+
     for (const l of logic) {
       l.execute(this.stack); // execute each teal opcode
     }
