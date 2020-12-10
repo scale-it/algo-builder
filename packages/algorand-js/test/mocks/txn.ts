42a186d73cc22afa2b765a55fef1ddfb71c89402
Ratik Jindal <ratikjindal21@gmail.com>
2020-12-10 12:12:32 +0530

    Interpreter - Add transaction context (#156)
    * remove deployer from mkTransaction
    
    * add transaction context
    
    * mock suggested params
    
    Co-authored-by: Robert Zaremba <robert@zaremba.ch>

---
+++ packages/algorand-js/test/mocks/txn.ts
@@ -0,0 +1,25 @@
+import { TxParams } from "algob/src/types";
+import type { SuggestedParams } from "algosdk";
+
+const ALGORAND_MIN_TX_FEE = 1000;
+const GENESIS_ID = 'testnet-v1.0';
+// testnet-v1.0 hash
+const GENESIS_HASH = 'SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI=';
+
+export function mockSuggestedParams (
+  payFlags: TxParams): SuggestedParams {
+  const s = {} as SuggestedParams;
+
+  s.flatFee = payFlags.totalFee !== undefined;
+  s.fee = payFlags.totalFee ?? payFlags.feePerByte ?? ALGORAND_MIN_TX_FEE;
+  if (s.flatFee) s.fee = Math.max(s.fee, ALGORAND_MIN_TX_FEE);
+
+  s.firstRound = payFlags.firstValid ?? 1;
+  s.lastRound = payFlags.firstValid === undefined || payFlags.validRounds === undefined
+    ? s.firstRound + 1000
+    : payFlags.firstValid + payFlags.validRounds;
+
+  s.genesisID = GENESIS_ID;
+  s.genesisHash = GENESIS_HASH;
+  return s;
+}
