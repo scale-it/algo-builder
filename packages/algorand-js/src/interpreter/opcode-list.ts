2bf41341a934c25a49d2f8ad7057ecec135dac47
Amit Yadav <amy29981@gmail.com>
2020-12-15 08:10:04 +0530

    fix

--- packages/algorand-js/src/interpreter/opcode-list.ts
+++ packages/algorand-js/src/interpreter/opcode-list.ts
@@ -239,7 +239,7 @@ export class Intcblock extends Op {
    */
   constructor (args: string[], line: number, interpreter: Interpreter) {
     super();
-    const intcblock: BigInt[];
+    const intcblock: Array<bigint> = [];
     for (const val of args) {
       assertOnlyDigits(val);
       intcblock.push(BigInt(val));
