b199df08da4974c557e81b4a12a47f9601b9838e
Amit Yadav <amy29981@gmail.com>
2020-12-16 17:45:32 +0530

    fix

--- packages/algorand-js/src/lib/compare.ts
+++ packages/algorand-js/src/lib/compare.ts
@@ -4,7 +4,12 @@
  * @param b unkown (array)
  */
 export function compareArray (a: unknown, b: unknown): Boolean {
-  if (Array.isArray(a) && Array.isArray(b)) {
+  if ((Array.isArray(a) && Array.isArray(b))) {
+    return a.length === b.length &&
+      a.every((v, i) => v === b[i]);
+  }
+
+  if (a instanceof Uint8Array && b instanceof Uint8Array) {
     return a.length === b.length &&
       a.every((v, i) => v === b[i]);
   }
