07d9f922a3ae25f8dc52278a49735a7626ca9b8d
Ratik Jindal <ratikjindal21@gmail.com>
2021-01-05 16:40:34 +0530

    rename algorand-js (#179)

--- packages/algorand-js/src/runtime/account.ts
+++ packages/algorand-js/src/runtime/account.ts
@@ -1,4 +1,3 @@
-import { StackElem, StoreAccount } from "algorand-js/src/types";
 import type {
   Account,
   AppLocalState,
@@ -12,6 +11,7 @@ import { TealError } from "../errors/errors";
 import { ERRORS } from "../errors/errors-list";
 import { compareArray } from "../lib/compare";
 import { assertValidSchema, getKeyValPair } from "../lib/stateful";
+import { StackElem, StoreAccount } from "../types";
 
 export class StoreAccountImpl implements StoreAccount {
   readonly account: Account;
