dc883a5944ca10fc6681472b32712e2803f60bcf
man-of-code <mratyunjay.soni@gmail.com>
2021-03-10 19:45:57 +0530

    Updated code, added docs

--- packages/algob/src/builtin-tasks/test.ts
+++ packages/algob/src/builtin-tasks/test.ts
@@ -1,3 +1,4 @@
+import * as fs from "fs";
 import Mocha from "mocha";
 
 import { task } from "../internal/core/config/config-env";
@@ -7,15 +8,22 @@ import { loadFilenames } from "./deploy";
 import { TASK_TEST } from "./task-names";
 
 async function runTests (config: AlgobConfig): Promise<void> {
-  const testFiles = loadFilenames(testsDirectory);
-  console.log("Test files:", testFiles);
-  const mocha = new Mocha(config.mocha);
-  // Adding test files to mocha object
-  testFiles.forEach((file) => mocha.addFile(file));
-  const testFailures = await new Promise<number>((resolve) => {
-    mocha.run(resolve);
-  });
-  console.log(testFailures);
+  if (!fs.existsSync(testsDirectory)) {
+    console.log("Test directory doesn't exists or exists with a different name. Please ensure that it's name is 'test'.");
+    return;
+  }
+  try {
+    const testFiles = loadFilenames(testsDirectory);
+    console.log("Test files:", testFiles);
+    const mocha = new Mocha(config.mocha);
+    // Adding test files to mocha object
+    testFiles.forEach((file) => mocha.addFile(file));
+    await new Promise<number>((resolve) => {
+      mocha.run(resolve);
+    });
+  } catch (error) {
+    console.log(error.message);
+  };
 }
 
 export default function (): void {
