1e70c12106ed12d7d9bc8027993f120ed0de2de0
amityadav0 <amy29981@gmail.com>
2021-04-26 11:42:50 +0530

    add update application in group transaction

--- packages/algob/src/lib/tx.ts
+++ packages/algob/src/lib/tx.ts
@@ -174,6 +174,16 @@ async function mkTx (
       txIdxMap.set(index, [name, { total: 1, decimals: 1, unitName: "MOCK" }]);
       break;
     }
+    case TransactionType.UpdateSSC: {
+      const name = txn.newApprovalProgram + "-" + txn.newClearProgram;
+      deployer.assertNoAsset(name);
+      const approval = await deployer.ensureCompiled(txn.newApprovalProgram);
+      const clear = await deployer.ensureCompiled(txn.newClearProgram);
+      txn.approvalProg = new Uint8Array(Buffer.from(approval.compiled, "base64"));
+      txn.clearProg = new Uint8Array(Buffer.from(clear.compiled, "base64"));
+      txIdxMap.set(index, [name, { total: 1, decimals: 1, unitName: "MOCK" }]);
+      break;
+    }
   }
   const suggestedParams = await getSuggestedParams(deployer.algodClient);
   return mkTransaction(txn,
