1121fcf1c081247260ed2799b4dfcc68bd11bb42
ratik <ratikjindal21@gmail.com>
2021-05-21 00:18:42 +0530

    add escrow contract and script in sample project

---
+++ packages/algob/sample-project/assets/escrow.py
@@ -0,0 +1,42 @@
+# Contract Account
+
+# Add directory to path so that algobpy can be imported
+import sys
+sys.path.insert(0,'.')
+
+from algobpy.parse import parseArgs
+from pyteal import *
+
+def escrow_contract(RECEIVER_ADDRESS):
+    '''
+    This contract represents an escrow account which only approves transfer to
+    address "RECEIVER_ADDRESS"
+    '''
+
+    # verify neither transaction
+    # contains a rekey
+    commons_checks = And(
+        Txn.rekey_to() == Global.zero_address(),
+        Txn.close_remainder_to() == Global.zero_address(),
+    )
+
+    program = And(
+        commons_checks,
+        Txn.receiver() == Addr(RECEIVER_ADDRESS)
+    )
+
+    return program
+
+
+if __name__ == "__main__":
+    # this is the default value (globalZeroAddress) of RECEIVER_ADDRESS. If template parameter
+    # via /script is not passed then this value will be used.
+    params = {
+        "RECEIVER_ADDRESS": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAY5HFKQ"
+    }
+
+    # Overwrite params if sys.argv[1] is passed
+    if(len(sys.argv) > 1):
+        params = parseArgs(sys.argv[1], params)
+
+    print(compileTeal(escrow_contract(params["RECEIVER_ADDRESS"]), Mode.Signature))
---
+++ packages/algob/sample-project/scripts/2-escrow-account.js
@@ -0,0 +1,21 @@
+/**
+ * In this script we will fund an escrow contract. The escrow contract
+ * ensures the payment is made out(from the escrow) to a specific receiver only.
+ * This receiver address is hardcoded in the smart contract, and can be passed
+ * dynamically to the contract using fundLsig function (passed as a template parameter)
+ */
+async function run (runtimeEnv, deployer) {
+  console.log('Sample script for escrow account started execution!');
+
+  // RECEIVER_ADDRESS is set in escrow.py when it is compiled from PyTEAL to TEAL
+  const templateParams = {
+    RECEIVER_ADDRESS: 'WHVQXVVCQAD7WX3HHFKNVUL3MOANX3BYXXMEEJEJWOZNRXJNTN7LTNPSTY'
+  };
+  await deployer.fundLsig('escrow.py',
+    { funder: deployer.accounts[0], fundingMicroAlgo: 20e6 }, {}, templateParams);
+
+  await deployer.addCheckpointKV('User Checkpoint Escrow', 'Fund Escrow Account');
+  console.log('Sample script for escrow account has finished execution!');
+}
+
+module.exports = { default: run };
