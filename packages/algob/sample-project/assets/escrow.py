ee605f078fdd14eb1c458aa292f8e22ed90d01eb
ratik <ratikjindal21@gmail.com>
2021-05-24 14:11:46 +0530

    rename parseArgs to parse_params

--- packages/algob/sample-project/assets/escrow.py
+++ packages/algob/sample-project/assets/escrow.py
@@ -4,7 +4,7 @@
 import sys
 sys.path.insert(0,'.')
 
-from algobpy.parse import parseArgs
+from algobpy.parse import parse_params
 from pyteal import *
 
 def escrow_contract(RECEIVER_ADDRESS):
@@ -35,6 +35,6 @@ if __name__ == "__main__":
 
     # Overwrite params if sys.argv[1] is passed
     if(len(sys.argv) > 1):
-        params = parseArgs(sys.argv[1], params)
+        params = parse_params(sys.argv[1], params)
 
     print(compileTeal(escrow_contract(params["RECEIVER_ADDRESS"]), Mode.Signature))
