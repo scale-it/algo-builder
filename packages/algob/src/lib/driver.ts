92961d07c44a97574119f00655b8e4aa5b2b885f
Ratik Jindal <ratikjindal21@gmail.com>
2021-05-27 16:44:14 +0530

    upgrade algosdk version (#350)

--- packages/algob/src/lib/driver.ts
+++ packages/algob/src/lib/driver.ts
@@ -5,11 +5,17 @@ import algosdk from "algosdk";
 import { ALGOB_CHAIN_NAME } from "../internal/constants";
 import { HttpNetworkConfig, KmdCfg, Network } from "../types";
 
+// appends https protocol to host if no protocol is added
+function _parseHost (host: string): string {
+  if (host.startsWith('http://') || host.startsWith('https://')) { return host; }
+  return `https://${host}`;
+}
+
 // @note: probably in the future we will remove this function and provide our own wrapper
 export function createClient (n: Network): algosdk.Algodv2 {
   if (n.name !== ALGOB_CHAIN_NAME) {
     const cfg = n.config as HttpNetworkConfig;
-    const algodv2 = new algosdk.Algodv2(cfg.token, cfg.host, cfg.port);
+    const algodv2 = new algosdk.Algodv2(cfg.token, _parseHost(cfg.host), cfg.port);
     algodv2.setIntEncoding('mixed'); // to support values > Number.MAX_SAFE_INTEGER
     return algodv2;
   }
@@ -17,5 +23,5 @@ export function createClient (n: Network): algosdk.Algodv2 {
 }
 
 export function createKmdClient (kmdCfg: KmdCfg): algosdk.Kmd {
-  return new algosdk.Kmd(kmdCfg.token, kmdCfg.host, kmdCfg.port);
+  return new algosdk.Kmd(kmdCfg.token, _parseHost(kmdCfg.host), kmdCfg.port);
 }
