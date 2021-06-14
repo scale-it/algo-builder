48d1ed6fb5c1863cfb39e07d36f97b41fb027787
amityadav0 <amy29981@gmail.com>
2021-06-14 10:11:27 +0530

    add execute transaction

--- packages/algob/src/internal/web-mode.ts
+++ packages/algob/src/internal/web-mode.ts
@@ -1,5 +1,5 @@
 import { mkTransaction, types as rtypes } from "@algo-builder/runtime";
-import { ConfirmedTxInfo, SuggestedParams, Transaction } from "algosdk";
+import algosdk, { ConfirmedTxInfo, SuggestedParams, Transaction } from "algosdk";
 
 import { AlgoSigner, AlgoSignerSendTx, AlgoSignerSignedTx, AlgoSignerToBeSignedTx } from "../types";
 
@@ -91,5 +91,42 @@ export class WebMode {
       ? s.lastRound : Number(userParams.firstValid) + Number(userParams.validRounds);
 
     return s;
+  }
+
+  /**
+ * Execute single transaction or group of transactions (atomic transaction)
+ * @param execParams transaction parameters or atomic transaction parameters
+ */
+  async executeTransaction (execParams: rtypes.ExecParams | rtypes.ExecParams[]):
+  Promise<ConfirmedTxInfo> {
+    let signedTxn;
+    let txns: Transaction[] = [];
+    if (Array.isArray(execParams)) {
+      if (execParams.length > 16) { throw new Error("Maximum size of an atomic transfer group is 16"); }
+
+      for (const [idx, txn] of execParams.entries()) {
+        txns.push(await mkTransaction(txn, await this.getSuggestedParams(txn.payFlags)));
+      }
+
+      txns = algosdk.assignGroupID(txns);
+      const binaryTxs = txns.map((txn: Transaction) => {
+        return txn.toByte();
+      });
+      const base64Txs = binaryTxs.map((txn: Uint8Array) => {
+        return this.algoSigner.encoding.msgpackToBase64(txn);
+      });
+      const toBeSignedTxns = base64Txs.map((txn: string) => {
+        return { txn: txn };
+      });
+      signedTxn = this.signTransaction(toBeSignedTxns);
+    } else {
+      const txn = await mkTransaction(execParams, await this.getSuggestedParams(execParams.payFlags));
+
+      const toBeSignedTxn = this.algoSigner.encoding.msgpackToBase64(txn.toByte());
+      signedTxn = this.signTransaction([{ txn: toBeSignedTxn }]);
+    }
+
+    const txInfo = await this.sendTransaction(signedTxn);
+    return await this.waitForConfirmation(txInfo.txId);
   }
 }
