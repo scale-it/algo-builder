073c071674299d15ecd4225d6233f8c39c72e022
Amit Yadav <amy29981@gmail.com>
2021-07-09 10:19:19 +0530

    add bond app checks

--- examples/bond/assets/bond-dapp-stateful.py
+++ examples/bond/assets/bond-dapp-stateful.py
@@ -40,8 +40,10 @@ def approval_program():
 
     update_issuer = Seq([
         Assert(
-            Txn.sender() == App.globalGet(store_manager),
-            basic_checks
+             And(
+                Txn.sender() == App.globalGet(store_manager),
+                basic_checks
+            )
         ),
         App.globalPut(issuer_address, Txn.application_args[1]),
         Return(Int(1))
@@ -49,15 +51,57 @@ def approval_program():
 
     on_issue = And(
         Gtxn[0].type_enum() == TxnType.AssetTransfer,
-        Gtxn[0].receiver() == App.globalGet(issuer_address),
-        Gtxn[0].application_id == App.globalGet(current_bond),
+        Gtxn[0].asset_receiver() == App.globalGet(issuer_address),
+        Gtxn[0].xfer_asset() == App.globalGet(current_bond),
         basic_checks
     )
 
     on_buy = Seq([
         And(
+            basic_checks,
+            # verify payment
+            Gtxn[0].type_enum() == TxnType.Payment,
+            # verify buying amount,
+            Gtxn[0].amount >= Add(Mul(Txn.application_args[1], App.globalGet(issue_price)), Gtxn[1].fee()),
+            # verify ASA transfer
+            Gtxn[1].type_enum() == TxnType.AssetTransfer,
+            # verify bond amount
+            Gtxn[1].asset_amount() == Txn.application_args[1],
+            Gtxn[1].xfer_asset() == App.globalGet(current_bond),
+            Gtxn[1].asset_receiver() == Gtxn[0].sender()
+        ),
+        App.globalPut(total, Add(App.globalGet(total), Gtxn[1].asset_amount()),
+        Return(Int(1))
+    ])
 
+    # store buyback address
+    create_buyback = Seq([
+        Assert(
+            And(
+                Txn.sender() == App.globalGet(store_manager),
+                basic_checks
+            )
+        ),
+        App.globalPut(Bytes("buyback"), Txn.application_args[1])
+    ])
+
+    on_exit = Seq([
+        Assert(
+            And(
+                basic_checks,
+                Gtxn[0].type_enum() == TxnType.AssetTransfer,
+                Gtxn[1].type_enum() == TxnType.Payment,
+                Gtxn[1].sender() == App.globalGet(Bytes("buyback")),
+                Gtxn[1].amount() == Sub(Mul(Gtxn[0].asset_amount(), App.globalGet(nominal_price)), Gtxn[1].fee),
+                Gtxn[1].receiver() == Txn.sender(),
+                Global.latest_timestamp() > App.globalGet(maturity_date)
+            )
         )
+        Return(Int(1))
+    ])
+
+    on_redeem_coupon = Seq([
+        
     ])
 
 program = Cond(
@@ -68,7 +112,10 @@ program = Cond(
         [Txn.on_completion() == OnComplete.OptIn, handle_optin],
         [Txn.application_args[0] == Bytes("update_issuer_address"), update_issuer],
         [Txn.application_args[0] == Bytes("issue"), on_issue],
-        [Txn.application_args[0] == Bytes("buy"), on_buy]
+        [Txn.application_args[0] == Bytes("buy"), on_buy],
+        [Txn.application_args[0] == Bytes("create_buyback"), create_buyback],
+        [Txn.application_args[0] == Bytes("exit"), on_exit],
+        [Txn.application_args[0] == Bytes("redeem_coupon"), on_redeem_coupon],
     )
 
     return program
