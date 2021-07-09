63fda9e5a51ca2f72f31adbda9b98a24d453b9dc
Amit Yadav <amy29981@gmail.com>
2021-07-09 22:01:32 +0530

    add create dex checks

--- examples/bond/assets/bond-dapp-stateful.py
+++ examples/bond/assets/bond-dapp-stateful.py
@@ -28,6 +28,7 @@ def approval_program():
         Txn.asset_close_to() == Global.zero_address()
     )
 
+    # initialize variables
     on_initialize = Seq([
         App.globalPut(store_manager, Txn.application_args[0]),
         App.globalPut(issue_price, Txn.application_args[1]),
@@ -40,6 +41,7 @@ def approval_program():
         Return(Int(1))
     ])
 
+    # only store manager can update issuer
     update_issuer = Seq([
         Assert(
              And(
@@ -51,6 +53,7 @@ def approval_program():
         Return(Int(1))
     ])
 
+    # only bon app manager can update issue price
     on_update_issue_price = Seq([
         Assert(
              And(
@@ -102,11 +105,16 @@ def approval_program():
         Assert(
             And(
                 basic_checks,
+                # verify first transaction is asset transfer
                 Gtxn[0].type_enum() == TxnType.AssetTransfer,
+                # verify second transaction is payment
                 Gtxn[1].type_enum() == TxnType.Payment,
+                # verify sender of first transaction is buyback address
                 Gtxn[1].sender() == App.globalGet(Bytes("buyback")),
+                # verify amount of algo received by buyer
                 Gtxn[1].amount() == Sub(Mul(Gtxn[0].asset_amount(), App.globalGet(nominal_price)), Gtxn[1].fee),
                 Gtxn[1].receiver() == Txn.sender(),
+                # verify maturity date is passed
                 Global.latest_timestamp() > App.globalGet(maturity_date)
             )
         )
@@ -140,8 +148,11 @@ def approval_program():
             ),
             Gtxn[0].type_enum() == TxnType.AssetTransfer,
             # transfer `balanceOf(issuer, B_i)`  of `B_{i+1}` from the creator to the `issuer`.
-            # burn `B_i` issuer bonds.
-            # 
+            # index 1 of Txn.accounts().
+            AssetHolding.balance(Int(1), Gtxn[1].xfer_asset()) == Gtxn[0].asset_amount(),
+            # burn `B_i` issuer bonds. send to null_address
+            Gtxn[1].type_enum() == TxnType.AssetTransfer,
+            AssetHolding.balance(Int(1), Gtxn[1].xfer_asset()) == Gtxn[1].asset_amount()
         ),
         # Increment `BondApp.epoch`
         App.globalPut(epoch, Add(App.globalGet(epoch), Int(1))),
@@ -150,18 +161,31 @@ def approval_program():
     ])
 
 program = Cond(
+        # Verfies that the application_id is 0, jumps to on_initialize.
         [Txn.application_id() == Int(0), on_initialize],
-        [Txn.on_completion() == OnComplete.UpdateApplication, handle_update],
+        # Verifies Update transaction, rejects it.
+        [Txn.on_completion() == OnComplete.UpdateApplication, Return(Int(0))],
+        # Verifies delete transaction, rejects it.
         [Txn.on_completion() == OnComplete.DeleteApplication, Return(Int(0))],
-        [Txn.on_completion() == OnComplete.CloseOut, handle_closeout],
-        [Txn.on_completion() == OnComplete.OptIn, handle_optin],
+        # Verifies closeout transaction, approves it.
+        [Txn.on_completion() == OnComplete.CloseOut, Return(Int(1))],
+        # Verifies opt-in transaction, approves it.
+        [Txn.on_completion() == OnComplete.OptIn, Return(Int(1))],
+        # Verifies update issuer address transaction, jumps to update_issuer branch.
         [Txn.application_args[0] == Bytes("update_issuer_address"), update_issuer],
+        # Verifies update issue, jumps to on_issue branch.
         [Txn.application_args[0] == Bytes("issue"), on_issue],
+        # Verifies buy transaction, jumps to on_buy branch.
         [Txn.application_args[0] == Bytes("buy"), on_buy],
+        # Verifies create buyback transaction, jumps to create_buyback branch.
         [Txn.application_args[0] == Bytes("create_buyback"), create_buyback],
+        # Verifies exit transaction, jumps to on_exit branch.
         [Txn.application_args[0] == Bytes("exit"), on_exit],
+        # Verifies redeem coupon transaction, jumps to on_redeem_coupon branch.
         [Txn.application_args[0] == Bytes("redeem_coupon"), on_redeem_coupon],
+        # Verifies update issue price transaction, jumps to on_update_issue_price branch.
         [Txn.application_args[0] == Bytes("update_issue_price"), on_update_issue_price],
+        # Verifies create dex transaction, jumps to on_create_dex branch.
         [Txn.application_args[0] == Bytes("create_dex"), on_create_dex],
     )
 
