c97ec8754d5e296d9524e5477423d46e18c292b6
ratik <ratikjindal21@gmail.com>
2021-09-04 06:11:28 +0530

    add clear_vote_record.js

---
+++ examples/dao/scripts/run/clear_vote_record.js
@@ -0,0 +1,31 @@
+const { executeTx } = require('./common/common.js');
+const { types } = require('@algo-builder/web');
+const { accounts, getDepositLsig } = require('./common/accounts.js');
+
+async function clearVoteRecord (deployer, voterAcc, proposalLsig) {
+  const daoAppInfo = deployer.getApp('dao-app-approval.py', 'dao-app-clear.py');
+
+  console.log(`* Clearing vote record of ${voterAcc.addr} from proposal ${proposalLsig.address()} *`);
+  const withdrawVoteParam = {
+    type: types.TransactionType.CallApp,
+    sign: types.SignType.SecretKey,
+    fromAccount: voterAcc,
+    appID: daoAppInfo.appID,
+    payFlags: { totalFee: 1000 },
+    appArgs: ['str:clear_vote_record'],
+    accounts: [proposalLsig.address()]
+  };
+
+  await executeTx(deployer, withdrawVoteParam);
+}
+
+async function run (runtimeEnv, deployer) {
+  const { _, __, voterA, voterB } = accounts(deployer);
+  const proposalLsig = await deployer.loadLogic('proposal-lsig.py');
+
+  // withdraw deposited votes by voterA & voterB (in ./deposit_vote.js)
+  await clearVoteRecord(deployer, voterA, proposalLsig);
+  await clearVoteRecord(deployer, voterB, proposalLsig);
+}
+
+module.exports = { default: run };
