17875da381635074e5f3feb5a857ac51b925a36f
Amit Yadav <amy29981@gmail.com>
2021-09-06 21:02:07 +0530

    Update packages/runtime/src/runtime.ts
    Co-authored-by: Ratik Jindal <ratikjindal21@gmail.com>

--- packages/runtime/src/runtime.ts
+++ packages/runtime/src/runtime.ts
@@ -579,7 +579,7 @@ export class Runtime {
    * @param to to address
    * @param amount amount of algo in microalgos
    */
-  fundAccount (from: RuntimeAccount, to: string, amount: number): void {
+  fundAccount (from: RuntimeAccount, to: AccountAddress, amount: number): void {
     const fundParam: ExecParams = {
       type: types.TransactionType.TransferAlgo,
       sign: types.SignType.SecretKey,
