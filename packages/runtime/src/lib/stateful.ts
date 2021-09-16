56a60adc362ea5a678c6aab1ca7d210916eba3fa
Ratik Jindal <ratikjindal21@gmail.com>
2021-09-16 17:57:36 +0530

    Tealv4: everything else (#460)
    * tealv4: everything else
    
    * update changelog
    
    * update comment

--- packages/runtime/src/lib/stateful.ts
+++ packages/runtime/src/lib/stateful.ts
@@ -3,6 +3,25 @@ import { modelsv2 } from "algosdk";
 import { RUNTIME_ERRORS } from "../errors/errors-list";
 import { RuntimeError } from "../errors/runtime-errors";
 import { StackElem } from "../types";
+import { MAX_KEY_BYTES, MAX_KEY_VAL_BYTES } from "./constants";
+
+/**
+ * Allow the combination of key-value pairs to a max size of 128 bytes.
+ * i.e (key.length + value.length <= 128)
+ * NOTE: The key is still restricted to 64 bytes or less.
+ * @param key stateful schema key
+ * @param value value at key (uint / bytes)
+ * https://developer.algorand.org/articles/introducing-algorand-virtual-machine-avm-09-release/
+ */
+function assertKeyValLengthValid (key: Uint8Array, value: StackElem): void {
+  let isSchemaInValid = false;
+  isSchemaInValid = (key.length > MAX_KEY_BYTES);
+  if (value instanceof Uint8Array && ((key.length + value.length) > MAX_KEY_VAL_BYTES)) {
+    isSchemaInValid = true;
+  }
+
+  if (isSchemaInValid) { throw new RuntimeError(RUNTIME_ERRORS.TEAL.INVALID_SCHEMA); }
+}
 
 /**
  * Description: assert if the given key-value pairs are valid by schema
@@ -14,6 +33,8 @@ export function assertValidSchema (
 ): void {
   let numUint = 0; let byteSlices = 0;
   keyValue.forEach((value, key) => {
+    const keyasByte = new Uint8Array(key.split(',').map(Number));
+    assertKeyValLengthValid(keyasByte, value);
     value instanceof Uint8Array ? byteSlices++ : numUint++;
   });
   if (numUint > schema.numUint || byteSlices > schema.numByteSlice) {
