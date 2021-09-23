4aa7ab9755df8dcdfc53c839e0d882807631ebe8
Amit Yadav <amy29981@gmail.com>
2021-09-23 12:21:59 +0530

    add execute transaction doc string

--- packages/algob/src/lib/tx.ts
+++ packages/algob/src/lib/tx.ts
@@ -300,6 +300,9 @@ export function signTransactionObject (
 
 /**
  * Execute single transaction or group of transactions (atomic transaction)
+ * executes `ExecParams` or `Transaction` Object, SDK Transaction object passed to this function
+ * will be signed and sent to network. User can use SDK functions to create transactions.
+ * Note: If passing transaction object a signer/s must be provided.
  * @param deployer Deployer
  * @param execParams transaction parameters or atomic transaction parameters
  * https://github.com/scale-it/algo-builder/blob/docs/docs/guide/execute-transaction.md
