ff3f3aa706dc833f06ee98f3e7f366d387eb8f87
Ashley Davis <ashley@codecapers.com.au>
2021-10-19 14:19:25 +1000

    Update packages/runtime/src/interpreter/interpreter.ts
    Co-authored-by: Robert Zaremba <robert@zaremba.ch>

--- packages/runtime/src/interpreter/interpreter.ts
+++ packages/runtime/src/interpreter/interpreter.ts
@@ -402,7 +402,8 @@ export class Interpreter {
    * @param runtime : runtime object
    * @param debugStack: if passed then TEAL Stack is logged to console after
    * each opcode execution (upto depth = debugStack)
-   * @returns The final result on the stack or undefined if nothing was on the stack.
+   * @returns The final result on the stack or undefined if nothing was on the stack. 
+   *    NOTE: program should fail there is no result (stack is empty after the execution) or the result is zero.
    */
   executeWithResult (program: string, mode: ExecutionMode, runtime: Runtime,
     debugStack?: number): StackElem | undefined {
