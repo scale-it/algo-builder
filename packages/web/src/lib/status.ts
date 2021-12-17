231e14fe60d1f362d4ce91a3d76f47e90589697b
Robert Zaremba <robert@zaremba.ch>
2021-12-17 11:55:06 +0100

    review updates

--- packages/web/src/lib/status.ts
+++ packages/web/src/lib/status.ts
@@ -1,9 +1,9 @@
 import type { Algodv2, modelsv2 } from "algosdk";
 
 /**
- * Returns `account` balance of `assetID`. Returns 0 if the account has not
- * opt-in to the given asset id.
- * @param deployer algob deployer
+ * Returns `account` balance of `assetID`. Returns 0 if the account is not
+ * opted-in to the given asset id.
+ * @param algodClient Algodv2 instance
  * @param accountAddress account to return assetholding info
  * @param assetID asset index
  */
