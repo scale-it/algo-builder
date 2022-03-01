eec662fe34fe4d3151b3d31e1d31d7e0134a7137
Robert Zaremba <robert@zaremba.ch>
2022-03-01 11:29:56 +0100

    fix

--- packages/runtime/src/interpreter/opcode-list.ts
+++ packages/runtime/src/interpreter/opcode-list.ts
@@ -223,7 +223,7 @@ export class Arg extends Op {
 
   execute (stack: TEALStack): void {
     this.checkIndexBound(
-      this.index, this.interpreter.runtime.ctx.args, this.line);
+      this.index, this.interpreter.runtime.ctx.args as Uint8Array[], this.line);
     const argN = this.assertBytes(this.interpreter.runtime.ctx.args?.[this.index], this.line);
     stack.push(argN);
   }
@@ -1616,7 +1616,7 @@ export class Global extends Op {
         break;
       }
       case 'CreatorAddress': {
-        const appID = this.interpreter.runtime.ctx.tx.apid;
+        const appID = this.interpreter.runtime.ctx.tx.apid as number;
         const app = this.interpreter.getApp(appID, this.line);
         result = decodeAddress(app.creator).publicKey;
         break;
@@ -4206,7 +4206,7 @@ export class Log extends Op {
     this.assertMinStackLen(stack, 1, this.line);
     const logByte = this.assertBytes(stack.pop(), this.line);
     const txID = this.interpreter.runtime.ctx.tx.txID;
-    const txReceipt = this.interpreter.runtime.ctx.state.txReceipts.get(txID);
+    const txReceipt = this.interpreter.runtime.ctx.state.txReceipts.get(txID) as TxReceipt;
     if (txReceipt.logs === undefined) { txReceipt.logs = []; }
 
     // max no. of logs exceeded
