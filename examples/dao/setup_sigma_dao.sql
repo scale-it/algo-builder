66100faf301d1f685196159a7942db78b7a0881a
Ravinda <ravindra.iitism@gmail.com>
2022-05-18 09:10:05 +0530

    asset_id and todo added

--- examples/dao/setup_sigma_dao.sql
+++ examples/dao/setup_sigma_dao.sql
@@ -6,15 +6,15 @@
 CREATE TABLE IF NOT EXISTS sigma_daos (
   app_id bigint PRIMARY KEY, -- application id
   app_params jsonb, -- application params with approval etc.
-  token_id bigint -- token id
+  asset_id bigint -- token id
 );
 
--- create a procedure to handle the trigger (update_app) action
+-- create a procedure to handle the trigger (sigmadao_trigger_fn) action
 CREATE OR REPLACE FUNCTION sigmadao_trigger_fn()
 RETURNS TRIGGER
 AS $$
 DECLARE
-    token_id bigint;
+    asset_id bigint;
 	log_sigma text := 'U2lnbWFEQU8gY3JlYXRlZA==';
 	i record;
 BEGIN
@@ -24,12 +24,13 @@ BEGIN
 		/*	sigma dao contrac check. Here 'dt' and 'lg' is a json object present
 			in each record of txn column */
 		IF i.txn::jsonb -> 'dt'->'lg' ?& ARRAY[log_sigma] THEN
-			-- store token id from db to above decalred variable token_id
-			SELECT index INTO token_id FROM public.asset ORDER BY index DESC LIMIT 1;
+			-- TODO: Fetch asset id from global state not from latest entry in asset table
+			-- store token id from db to above decalred variable asset_id
+			SELECT index INTO asset_id FROM public.asset ORDER BY index DESC LIMIT 1;
 			-- insert values in sigma_daos table
 			INSERT INTO public.sigma_daos (
-			app_id, app_params, token_id)
-			VALUES (NEW.asset, NEW.txn, token_id);
+			app_id, app_params, asset_id)
+			VALUES (NEW.asset, NEW.txn, asset_id);
 			EXIT;
 		END IF;
     END LOOP;
@@ -37,7 +38,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- create a trigger (update_app)
+-- create a trigger (sigmadao_trigger)
 -- Event: INSERT on publoc.txn relation
 CREATE TRIGGER sigmadao_trigger
 AFTER INSERT ON public.txn FOR EACH ROW
