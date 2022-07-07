7242ca5750e0598f060b8c72b5b9f30bfc1e3157
Ravinda <ravindra.iitism@gmail.com>
2022-07-07 11:28:57 +0530

    suggestions

--- examples/dao/setup_sigma_dao.sql
+++ examples/dao/setup_sigma_dao.sql
@@ -132,13 +132,13 @@ DECLARE
 	filter_past INT := 4; -- filter type -> past
 BEGIN
 	IF (filterType = filter_ongoing) THEN
-		RETURN QUERY SELECT * FROM sigma_dao_proposals WHERE app=appId AND current_time BETWEEN sigma_dao_proposals.voting_start AND sigma_dao_proposals.voting_end ORDER BY voting_start;
+		RETURN QUERY SELECT * FROM sigma_dao_proposals WHERE app = appId AND current_time BETWEEN sigma_dao_proposals.voting_start AND sigma_dao_proposals.voting_end ORDER BY voting_start;
 	ELSIF (filterType = filter_active) THEN
-		RETURN QUERY SELECT * FROM sigma_dao_proposals WHERE app=appId AND current_time < voting_end ORDER BY voting_start;
+		RETURN QUERY SELECT * FROM sigma_dao_proposals WHERE app = appId AND current_time < voting_end ORDER BY voting_start;
 	ELSIF (filterType = filter_past) THEN
-		RETURN QUERY SELECT * FROM sigma_dao_proposals WHERE app=appId AND sigma_dao_proposals.voting_end < current_time ORDER BY voting_end DESC;
+		RETURN QUERY SELECT * FROM sigma_dao_proposals WHERE app = appId AND sigma_dao_proposals.voting_end < current_time ORDER BY voting_end DESC;
 	ELSE
-		RETURN QUERY SELECT * FROM sigma_dao_proposals WHERE app=appId;
+		RETURN QUERY SELECT * FROM sigma_dao_proposals WHERE app = appId;
 	END IF;
 END;
 $$ LANGUAGE plpgsql STABLE;
