7e7a003c73f39896010819780911289d0ba5771a
Ravinda <ravindra.iitism@gmail.com>
2022-09-26 16:22:28 +0530

    Feature: Add support loadLogic for *.teal program and SCParams

--- packages/runtime/test/src/logicsig.ts
+++ packages/runtime/test/src/logicsig.ts
@@ -1,14 +1,16 @@
 import { decodeAddress, multisigAddress, MultisigMetadata } from "algosdk";
 import { assert } from "chai";
-
+const { convert } = require("@algo-builder/algob");
 import { AccountStore } from "../../src/account";
 import { RUNTIME_ERRORS } from "../../src/errors/errors-list";
 import { Runtime } from "../../src/runtime";
 import { useFixture } from "../helpers/integration";
 import { expectRuntimeError } from "../helpers/runtime-errors";
+const { types } = require("@algo-builder/web");
 
 const programName = "escrow.teal";
 const multiSigProg = "sample-asc.teal";
+const crowdFundEscrow = "crowdFundEscrow.teal";
 
 describe("Logic Signature", () => {
 	useFixture("escrow-account");
@@ -16,12 +18,49 @@ describe("Logic Signature", () => {
 	let bob: AccountStore;
 	let runtime: Runtime;
 	let johnPk: Uint8Array;
+	let goal: number;
+	let crowdfundApprovalFileName: string;
+	let crowdFundClearFileName: string;
+	let now: Date;
+	let beginDate: Date;
+	let endDate: Date;
+	let fundCloseDate: Date;
+	let creationArgs: Array<{}>;
+	let appDefinition: any;
+	let applicationId: any
 
 	before(() => {
+		goal = 0.01e6;
+		crowdfundApprovalFileName = "crowdFundApproval.teal";
+		crowdFundClearFileName = "crowdFundClear.teal";
 		john = new AccountStore(10);
-		bob = new AccountStore(100);
+		bob = new AccountStore(10e6);
 		runtime = new Runtime([john, bob]);
 		johnPk = decodeAddress(john.address).publicKey;
+		now = new Date();
+		beginDate = endDate = fundCloseDate = now
+		beginDate.setSeconds(now.getSeconds() + 2);
+		endDate.setSeconds(now.getSeconds() + 12000);
+		fundCloseDate.setSeconds(fundCloseDate.getSeconds() + 120000);
+
+		creationArgs = [
+			convert.uint64ToBigEndian(beginDate.getTime()),
+			convert.uint64ToBigEndian(endDate.getTime()),
+			`int:${goal}`, // args similar to `goal --app-arg ..` are also supported
+			convert.addressToPk(bob.address),
+			convert.uint64ToBigEndian(fundCloseDate.getTime()),
+		];
+
+		appDefinition = {
+			appName: "crowdfundingApp",
+			metaType: types.MetaType.FILE,
+			approvalProgramFilename: crowdfundApprovalFileName,
+			clearProgramFilename: crowdFundClearFileName,
+			localInts: 1,
+			localBytes: 0,
+			globalInts: 5,
+			globalBytes: 3,
+		};
 	});
 
 	it("john should be able to create a delegated signature", () => {
@@ -43,6 +82,35 @@ describe("Logic Signature", () => {
 	it("should handle contract lsig (escrow account) verification correctly", () => {
 		const lsig = runtime.loadLogic(programName);
 
+		let result = lsig.lsig.verify(decodeAddress(lsig.address()).publicKey);
+		assert.equal(result, true);
+
+		result = lsig.lsig.verify(johnPk);
+		assert.equal(result, false);
+	});
+
+	it("should handle contract lsig (escrow account) verification correctly with empty smart contract parmas", () => {
+		// empty smart contract param with teal
+		const lsig = runtime.loadLogic(programName, {});
+
+		let result = lsig.lsig.verify(decodeAddress(lsig.address()).publicKey);
+		assert.equal(result, true);
+
+		result = lsig.lsig.verify(johnPk);
+		assert.equal(result, false);
+	});
+
+	it("should handle contract lsig (crowd fund escrow account) verification correctly with non-empty smart contract parmas", () => {
+		// create application
+		applicationId = runtime.deployApp(
+			bob.account,
+			{ ...appDefinition, appArgs: creationArgs },
+			{}
+		).appID;
+
+		assert(applicationId);
+		// setup escrow account
+		const lsig = runtime.loadLogic(crowdFundEscrow, { APP_ID: applicationId });
 		let result = lsig.lsig.verify(decodeAddress(lsig.address()).publicKey);
 		assert.equal(result, true);
 
