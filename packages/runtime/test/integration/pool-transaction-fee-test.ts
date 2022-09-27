b367c9fb8e065d27f952c2137f46164bf93a40df
Ravinda <ravindra.iitism@gmail.com>
2022-09-27 15:28:52 +0530

    more tests

--- packages/runtime/test/integration/pool-transaction-fee-test.ts
+++ packages/runtime/test/integration/pool-transaction-fee-test.ts
@@ -3,23 +3,33 @@ import { assert } from "chai";
 
 import { RUNTIME_ERRORS } from "../../src/errors/errors-list";
 import { AccountStore, Runtime } from "../../src/index";
-import { ALGORAND_ACCOUNT_MIN_BALANCE } from "../../src/lib/constants";
 import { useFixture } from "../helpers/integration";
 import { expectRuntimeError } from "../helpers/runtime-errors";
 
 describe("Pooled Transaction Fees Test", function () {
-	useFixture("app-update");
-	const minBalance = ALGORAND_ACCOUNT_MIN_BALANCE;
-	let john = new AccountStore(1e30);
-	let alice = new AccountStore(minBalance);
-	let bob = new AccountStore(minBalance);
-	let elon = new AccountStore(minBalance);
-
+	useFixture("deploy-asa");
+	const initialBalance = 1e30;
+	let john: AccountStore;
+	let bob: AccountStore;
+	let elon: AccountStore;
+	let alice: AccountStore;
+	let assetId: number;
 	let runtime: Runtime;
 
 	this.beforeEach(async function () {
-		runtime = new Runtime([john, alice, bob, elon]); // setup test
+		john = new AccountStore(initialBalance, "john");
+		bob = new AccountStore(initialBalance, "bob");
+		alice = new AccountStore(initialBalance, "alice");
+		elon = new AccountStore(initialBalance, "elon");
+		runtime = new Runtime([john, bob, alice, elon]); // setup test
 	});
+
+	function setupAsset(): void {
+		// create asset
+		assetId = runtime.deployASA("asa", {
+			creator: { ...john.account, name: "john" },
+		}).assetIndex;
+	}
 
 	// helper function
 	function syncAccounts(): void {
@@ -55,8 +65,8 @@ describe("Pooled Transaction Fees Test", function () {
 		runtime.executeTx(groupTx);
 
 		syncAccounts();
-		assert.equal(bob.balance(), BigInt(minBalance + amount));
-		assert.equal(alice.balance(), BigInt(minBalance));
+		assert.equal(bob.balance(), BigInt(initialBalance) + BigInt(amount));
+		assert.equal(alice.balance(), BigInt(initialBalance));
 		assert.equal(john.balance(), initialBalance - BigInt(amount) - 2000n);
 	});
 
@@ -89,7 +99,7 @@ describe("Pooled Transaction Fees Test", function () {
 		);
 	});
 
-	it("Should unfunded accounts be able to issue transactions, through execute transaction", () => {
+	it("Should unfunded accounts be able to issue transactions of group size 3", () => {
 		const amount = 4000;
 		const fee = 3000;
 		const johnInitialBalance = john.balance();
@@ -124,8 +134,50 @@ describe("Pooled Transaction Fees Test", function () {
 		runtime.executeTx(groupTx);
 
 		syncAccounts();
-		assert.equal(john.balance(), johnInitialBalance - BigInt(fee));
-		assert.equal(alice.balance(), BigInt(minBalance));
-		assert.equal(elon.balance(), BigInt(minBalance));
+		assert.equal(john.balance(), BigInt(initialBalance) - BigInt(fee));
+		assert.equal(alice.balance(), BigInt(initialBalance));
+		assert.equal(elon.balance(), BigInt(initialBalance));
 	});
+
+	it("Should unfunded accounts be able to issue transactions and opt-in", () => {
+		setupAsset();
+		const amount = 4000;
+		const fee = 3000;
+		// group with fee distribution
+		const groupTx: types.ExecParams[] = [
+			{
+				type: types.TransactionType.TransferAlgo,
+				sign: types.SignType.SecretKey,
+				fromAccount: john.account,
+				toAccountAddr: alice.address,
+				amountMicroAlgos: amount,
+				payFlags: { totalFee: 0 }, // with 0 txn fee
+			},
+			{
+				type: types.TransactionType.TransferAlgo,
+				sign: types.SignType.SecretKey,
+				fromAccount: alice.account,
+				toAccountAddr: elon.address,
+				amountMicroAlgos: amount,
+				payFlags: { totalFee: fee }, // this covers fee of entire group txns
+			},
+			{
+				type: types.TransactionType.OptInASA,
+				sign: types.SignType.SecretKey,
+				fromAccount: elon.account,
+				assetID: assetId,
+				payFlags: { totalFee: 0 }, // with 0 txn fee
+			},
+		];
+
+		runtime.executeTx(groupTx);
+
+		syncAccounts();
+		assert.equal(john.balance(), BigInt(initialBalance) - BigInt(amount));
+		assert.equal(alice.balance(), BigInt(initialBalance) - BigInt(fee));
+		assert.equal(elon.balance(), BigInt(initialBalance) + BigInt(amount));
+		// verify holding
+		assert.isDefined(john.getAssetHolding(assetId));
+	});
+
 });
