96f4f82f86fdeb240842d54441e4eed4aeffea74
Ravinda <ravindra.iitism@gmail.com>
2022-09-28 18:13:56 +0530

    lint

--- packages/runtime/src/lib/load-program.ts
+++ packages/runtime/src/lib/load-program.ts
@@ -7,7 +7,8 @@ import { ASSETS_DIR, PyCompileOp, pyExt, tealExt } from "./pycompile-op";
 /**
  * returns program TEAL code.
  * @param fileName filename in /assets. Must end with .teal OR .py
- * @param scInitParam smart contract template parameters, used to set hardcoded values in .py or .teal smart contract.
+ * @param scInitParam smart contract template parameters, used to set hardcoded values
+ * in .py or .teal smart contract.
  * @param logs only show logs on console when set as true. By default this value is true
  */
 export function getProgram(fileName: string, scInitParam?: SCParams, logs = true): string {
