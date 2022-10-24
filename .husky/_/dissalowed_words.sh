c39d07d718586fc1d553c9c9423ca5c80e1de8e1
Stanislaw Czembor <stanislaw.czembor@gmail.com>
2022-10-24 13:18:50 +0200

    test

--- .husky/_/dissalowed_words.sh
+++ .husky/_/dissalowed_words.sh
@@ -3,9 +3,9 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 PURPLE='\033[0;35m'
 YELLOW='\033[1;33m'
-status=0
 set -o pipefail
 func() {
+    status=0
     git diff --cached --name-status | while read x file; do
     if [ "$x" == 'D' ]; then continue; fi
     if ! [[ "${file: -3}" == ".ts" ]] || [[ "${file: -3}" == ".js" ]]; then continue; fi
