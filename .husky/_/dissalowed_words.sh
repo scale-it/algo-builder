520373bdc9ef126dbfaaf83924061487def0a514
Stanislaw Czembor <stanislaw.czembor@gmail.com>
2022-10-24 12:05:30 +0200

    test

--- .husky/_/dissalowed_words.sh
+++ .husky/_/dissalowed_words.sh
@@ -6,7 +6,7 @@ status=0;
 
 git diff --cached --name-status | while read x file; do
     if [ "$x" == 'D' ]; then continue; fi
-    if [[ ${file: -3} != ".ts" || ${file: -3} != ".js" ]]; then continue; fi
+    if [[ ${file: -3} != ".ts"] || [${file: -3} != ".js" ]]; then continue; fi
     if egrep -q "\.only\(" $file ; then
         echo "${RED}ERROR:${NC} Disallowed expression ${YELLOW}.only()${NC} in file: ${PURPLE}${file}${NC}"
         status=1;
