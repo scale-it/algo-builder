7fa00cdddb5aa1c34740121310ffa0d5cfc8adcc
Stanislaw Czembor <stanislaw.czembor@gmail.com>
2022-10-24 11:46:38 +0200

    test

--- .husky/_/dissalowed_words.sh
+++ .husky/_/dissalowed_words.sh
@@ -1,15 +1,18 @@
-RED='\033[0;31m'
-NC='\033[0m' # No Color
-PURPLE='\033[0;35m'
-YELLOW='\033[1;33m'
-status=0;
+# RED='\033[0;31m'
+# NC='\033[0m' # No Color
+# PURPLE='\033[0;35m'
+# YELLOW='\033[1;33m'
+# status=0;
+
+# git diff --cached --name-status | while read x file; do
+#     if [ "$x" == 'D' ]; then continue; fi
+#         if egrep -q "\.only\(" $file ; then
+#             echo "${RED}ERROR:${NC} Disallowed expression ${YELLOW}.only()${NC} in file: ${PURPLE}${file}${NC}"
+#             status=1;
+#         fi
+# done
+# [ $status -eq 1 ]  || exit 1
 
 git diff --cached --name-status | while read x file; do
-    if [ "$x" == 'D' ]; then continue; fi
-        if egrep -q "\.only\(" $file ; then
-            echo "${RED}ERROR:${NC} Disallowed expression ${YELLOW}.only()${NC} in file: ${PURPLE}${file}${NC}"
-            status=1;
-        fi
-done
-echo "value of status ${status}"
-exit $status
+    echo "${file}"
+done || exit 0
