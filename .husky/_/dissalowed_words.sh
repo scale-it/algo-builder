0a9838fd0b2ea3a5cef4e148ee296e4601db6be5
Stanislaw Czembor <stanislaw.czembor@gmail.com>
2022-10-24 10:52:26 +0200

    test

--- .husky/_/dissalowed_words.sh
+++ .husky/_/dissalowed_words.sh
@@ -1,12 +1,10 @@
 disallowed="\.only\("
 
+
 git diff --cached --name-status | while read x file; do
         if [ "$x" == 'D' ]; then continue; fi
-        for word in $disallowed
-        do
             if egrep $word $file ; then
                 echo "ERROR: Disallowed expression \"${word}\" in file: ${file}"
                 exit 1
             fi
-        done
 done || exit $?
