f6a21bd1bb465c79c4a6b0dd7c70f5219b116519
Stanislaw Czembor <stanislaw.czembor@gmail.com>
2022-10-24 11:28:07 +0200

    updated script

--- .husky/_/dissalowed_words.sh
+++ .husky/_/dissalowed_words.sh
@@ -1,7 +1,12 @@
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+PURPLE='\033[0;35m'
+YELLOW='\033[1;33m'
+
 git diff --cached --name-status | while read x file; do
-        if [ "$x" == 'D' ]; then continue; fi
-        if grep -rq --include $file  it.only ; then
-                echo 'ERROR: Dissalowed it.only() in your tests in file: ${file}'
-                exit 1
+    if [ "$x" == 'D' ]; then continue; fi
+        if egrep -q "\.only\(" $file ; then
+            echo "${RED}ERROR:${NC} Disallowed expression ${YELLOW}it.only()${NC} in file: ${PURPLE}${file}${NC}"
+            exit 1
         fi
 done || exit $?
