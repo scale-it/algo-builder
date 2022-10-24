49ccedcb98f48812d1102b8f5c4e6d488bca3f5b
Stanislaw Czembor <stanislaw.czembor@gmail.com>
2022-10-24 13:17:40 +0200

    test

--- .husky/_/dissalowed_words.sh
+++ .husky/_/dissalowed_words.sh
@@ -1,16 +1,19 @@
+#!/bin/bash
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 PURPLE='\033[0;35m'
 YELLOW='\033[1;33m'
-failing=0;
-
-git diff --cached --name-status | while read x file; do
+status=0
+set -o pipefail
+func() {
+    git diff --cached --name-status | while read x file; do
     if [ "$x" == 'D' ]; then continue; fi
-    if ! [[ ${file: -3} == ".ts" ]]; then continue; fi
-    if egrep -q "\.only\(" $file ; then
+    if ! [[ "${file: -3}" == ".ts" ]] || [[ "${file: -3}" == ".js" ]]; then continue; fi
+    if gerp -E -q "\.only\(" "$file" ; then
         echo "${RED}ERROR:${NC} Disallowed expression ${YELLOW}.only()${NC} in file: ${PURPLE}${file}${NC}"
-        ((failing+=1));
+        status=1;
     fi
 done
-if [ $failing != 0 ]; then exit 1; else exit 0; fi
-exit;
+exit $status
+}
+func;
