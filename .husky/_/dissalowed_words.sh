b44fe3e49ce9c37f3f75e17b97104e46627fc687
Stanislaw Czembor <stanislaw.czembor@gmail.com>
2022-10-25 10:35:37 +0200

    add line number to error output

--- .husky/_/dissalowed_words.sh
+++ .husky/_/dissalowed_words.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 COLOR_REST="$(tput sgr0)"
-COLOR_MAGENTA="$(tput setaf 4)"
+COLOR_MAGENTA="$(tput setaf 5)"
 COLOR_RED="$(tput setaf 1)"
 COLOR_YELLOW="$(tput setaf 3)"
 status=0
@@ -8,21 +8,10 @@ status=0
 for file in $(git diff --cached --name-only --diff-filter=ACMT);
 do
     if [ "${file: -3}" != ".ts" ] && [ "${file: -3}" != ".js" ]; then continue; fi
-    grep -in -E "\.only\("  "$file" | awk -F: '{print $2" - Line number : "$1}'
-    # if grep -E -q "\.only\(" "$file" ; then
-    #     printf "%40s\n" "${COLOR_RED}ERROR:${COLOR_REST} Disallowed expression${COLOR_YELLOW} .only()${COLOR_REST} in file: $COLOR_MAGENTA${file}$COLOR_REST"
-    #     status=1 
-    # fi
+    for line in $(grep -in -E "\.only\("  "$file" | cut -f1 -d:);
+    do
+        printf "%40s\n" "${COLOR_RED}ERROR:${COLOR_REST} Disallowed expression${COLOR_YELLOW} .only()${COLOR_REST} in file: $COLOR_MAGENTA${line}:${file}$COLOR_REST"
+        status=1 
+    done
 done
 exit $status
-
-
-# while read x file; do
-#     if [ "$x" == 'D' ]; then continue; fi
-#     if [ "${file: -3}" != ".ts" ] && [ "${file: -3}" != ".js" ]; then continue; fi
-#     if grep -E -q "\.only\(" "$file" ; then
-#         printf "%40s\n" "${COLOR_RED}ERROR:${COLOR_REST} Disallowed expression${COLOR_YELLOW} .only()${COLOR_REST} in file: $COLOR_MAGENTA${file}$COLOR_REST"
-#         status=1 
-#     fi
-# done < <(git diff --cached --name-status);
-# exit $status
