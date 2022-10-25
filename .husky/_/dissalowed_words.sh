965c223f47c4662fcc288d40165cbff5ebe6da96
Stanislaw Czembor <stanislaw.czembor@gmail.com>
2022-10-25 10:19:09 +0200

    test

--- .husky/_/dissalowed_words.sh
+++ .husky/_/dissalowed_words.sh
@@ -1,16 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 COLOR_REST="$(tput sgr0)"
 COLOR_MAGENTA="$(tput setaf 4)"
 COLOR_RED="$(tput setaf 1)"
 COLOR_YELLOW="$(tput setaf 3)"
 status=0
-set +o posix
-while read x file; do
-    if [ "$x" == 'D' ]; then continue; fi
+
+for file in $(git diff --cached --name-only --diff-filter=ACMT);
+do
     if [ "${file: -3}" != ".ts" ] && [ "${file: -3}" != ".js" ]; then continue; fi
-    if grep -E -q "\.only\(" "$file" ; then
-        printf "%40s\n" "${COLOR_RED}ERROR:${COLOR_REST} Disallowed expression${COLOR_YELLOW} .only()${COLOR_REST} in file: $COLOR_MAGENTA${file}$COLOR_REST"
-        status=1 
-    fi
-done < <(git diff --cached --name-status);
+    grep -in -E "\.only\("  file | awk -F: '{print $2" - Line number : "$1}'
+    # if grep -E -q "\.only\(" "$file" ; then
+    #     printf "%40s\n" "${COLOR_RED}ERROR:${COLOR_REST} Disallowed expression${COLOR_YELLOW} .only()${COLOR_REST} in file: $COLOR_MAGENTA${file}$COLOR_REST"
+    #     status=1 
+    # fi
+done
 exit $status
+
+
+# while read x file; do
+#     if [ "$x" == 'D' ]; then continue; fi
+#     if [ "${file: -3}" != ".ts" ] && [ "${file: -3}" != ".js" ]; then continue; fi
+#     if grep -E -q "\.only\(" "$file" ; then
+#         printf "%40s\n" "${COLOR_RED}ERROR:${COLOR_REST} Disallowed expression${COLOR_YELLOW} .only()${COLOR_REST} in file: $COLOR_MAGENTA${file}$COLOR_REST"
+#         status=1 
+#     fi
+# done < <(git diff --cached --name-status);
+# exit $status
--- examples/dao/test/happy-path.js
+++ examples/dao/test/happy-path.js
@@ -14,7 +14,7 @@ const {
 	mkCloseProposalTx,
 } = require("../scripts/run/common/tx-params");
 
-describe("DAO - Happy Paths", function () {
+describe.only("DAO - Happy Paths", function () {
 	let master, creator, proposer, voterA, voterB;
 	let daoFundLsigAcc, proposalLsigAcc;
 	let ctx;
