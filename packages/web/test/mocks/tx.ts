a73ef8b803ee78557c610172959b412f3217f906
MEGHA <100185149+Megha-Dev-19@users.noreply.github.com>
2022-10-31 18:32:02 +0530

    removed unnecessary types

--- packages/web/test/mocks/tx.ts
+++ packages/web/test/mocks/tx.ts
@@ -1,4 +1,4 @@
-import algosdk, { SuggestedParams } from "algosdk";
+import algosdk from "algosdk";
 
 export const txObject = {
 	name: "Transaction",
@@ -36,87 +36,4 @@ const alicemne = "brand globe reason guess allow wear roof leisure season coin o
 export const receiverAccount = {
 	addr: "EDXG4GGBEHFLNX6A7FGT3F6Z3TQGIU6WVVJNOXGYLVNTLWDOCEJJ35LWJY",
 	sk: algosdk.mnemonicToSecretKey(alicemne).sk,
-}
-
-const addr = algosdk.decodeAddress(senderAccount.addr);
-
-// MOCK Algorand Encoded Transaction
-export const encodedTxnObject: algosdk.EncodedTransaction = {
-	snd: Buffer.from(addr.publicKey),
-	rcv: Buffer.from(addr.publicKey),
-	arcv: Buffer.from(addr.publicKey),
-	fee: 1000,
-	amt: 20200,
-	aamt: 100,
-	fv: 258820,
-	lv: 259820,
-	note: Buffer.from("Note"),
-	gen: "default-v1",
-	gh: Buffer.from("default-v1"),
-	lx: Buffer.from(""),
-	aclose: Buffer.from(addr.publicKey),
-	close: Buffer.from(addr.publicKey),
-	votekey: Buffer.from("voteKey"),
-	selkey: Buffer.from("selectionKey"),
-	votefst: 123,
-	votelst: 345,
-	votekd: 1234,
-	xaid: 1101,
-	caid: 101,
-	apar: {
-		t: 10,
-		dc: 0,
-		df: false,
-		m: Buffer.from(addr.publicKey),
-		r: Buffer.from(addr.publicKey),
-		f: Buffer.from(addr.publicKey),
-		c: Buffer.from(addr.publicKey),
-		un: "tst",
-		an: "testcoin",
-		au: "testURL",
-		am: Buffer.from("test-hash"),
-	},
-	fadd: Buffer.from(addr.publicKey),
-	faid: 202,
-	afrz: false,
-	apid: 1828,
-	apan: 0,
-	apap: Buffer.from("approval"),
-	apsu: Buffer.from("clear"),
-	apaa: [Buffer.from("arg1"), Buffer.from("arg2")],
-	apat: [],
-	apfa: [1828, 1002, 1003],
-	apas: [2001, 2002, 2003],
-	type: "pay",
-	apls: {
-		nui: 1,
-		nbs: 2,
-	},
-	apgs: {
-		nui: 3,
-		nbs: 4,
-	},
-	rekey: Buffer.from(addr.publicKey),
-	grp: Buffer.from("group"),
-	apep: 1,
-	nonpart: true,
-};
-
-export const mockSuggestedParam: SuggestedParams = {
-	flatFee: false,
-	fee: 100,
-	firstRound: 2,
-	lastRound: 100,
-	genesisID: "testnet-v1.0",
-	genesisHash: "SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI=",
-};
-
-export const txnReceiptMock = {
-	txID: "1",
-	"confirmed-round": 1,
-	"asset-index": 1,
-	"application-index": 1,
-	txn: {
-		txn: encodedTxnObject,
-	}
 }
