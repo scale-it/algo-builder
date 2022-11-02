86431468803d2fc6472714e50c6f478d768bfa49
sczembor <43810037+sczembor@users.noreply.github.com>
2022-11-02 16:18:28 +0100

    add runtime genesis hash

--- packages/web/src/lib/constants.ts
+++ packages/web/src/lib/constants.ts
@@ -12,3 +12,6 @@ export const betanetURL = "https://node.betanet.algoexplorerapi.io";
 export const mainnetGenesisHash = "wGHE2Pwdvd7S12BL5FaOP20EGYesN73ktiC1qzkkit8=";
 export const testnetGenesisHash = "SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI=";
 export const betanetGenesisHash = "mFgazF+2uRS1tMiL9dsj01hJGySEmPN28B/TjjvpVW0=";
+
+//runtime network
+export const runtimeGenesisHash = "runtime";
