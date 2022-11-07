937c06569cd8f1334cde415e0a3da52d0444942a
sczembor <43810037+sczembor@users.noreply.github.com>
2022-11-07 23:50:10 +0100

    cleanup

--- .husky/_/dissalowed_words.sh
+++ .husky/_/dissalowed_words.sh
@@ -5,31 +5,13 @@ COLOR_RED="$(tput setaf 1)"
 COLOR_YELLOW="$(tput setaf 3)"
 status=0
 
-echo "################################test"
-
-files=$(git diff --name-only)
-echo "before loop"
-echo "$files"
-for file in $files; do
-  echo "am i even here?"
-  echo $file
+for file in $(git diff --name-only --diff-filter=ACMT);
+do
+    if [ "${file: -3}" != ".ts" ] && [ "${file: -3}" != ".js" ]; then continue; fi
+    for line in $(grep -in -E "\.only\("  "$file" | cut -f1 -d:);
+    do
+        printf "%40s\n" "${COLOR_RED}ERROR:${COLOR_REST} Disallowed expression${COLOR_YELLOW} .only()${COLOR_REST} in file: $COLOR_MAGENTA${line}:${file}$COLOR_REST"
+        status=1 
+    done
 done
-   
-
-
-
-
-
-
-   
-# for file in $(git diff --name-only);
-# do
-#     echo "$file"
-    # if [ "${file: -3}" != ".ts" ] && [ "${file: -3}" != ".js" ]; then continue; fi
-    # for line in $(grep -in -E "\.only\("  "$file" | cut -f1 -d:);
-    # do
-    #     printf "%40s\n" "${COLOR_RED}ERROR:${COLOR_REST} Disallowed expression${COLOR_YELLOW} .only()${COLOR_REST} in file: $COLOR_MAGENTA${line}:${file}$COLOR_REST"
-    #     status=1 
-    # done
-# done
 exit $status
--- packages/runtime/test/src/interpreter/interpreter.ts
+++ packages/runtime/test/src/interpreter/interpreter.ts
@@ -213,11 +213,12 @@ describe("Interpreter", function () {
 			});
 		});
 
-		describe.only("Immutable access to foreign app accounts", function () {
+		describe("Immutable access to foreign app accounts", function () {
 			this.beforeEach(function () {
 				setUpInterpreter(7, 1e9);
 			});
-			it.only("Should allow to read the account state of foreign apps based on app id", function () {
+			
+			it("Should allow to read the account state of foreign apps based on app id", function () {
 				const prog = `
 				txn Applications 2
 				store 1
@@ -233,7 +234,7 @@ describe("Interpreter", function () {
 				assert.doesNotThrow(() => executeTEAL(prog));
 			});
 
-			it.only("Should allow to read the account state of foreign apps based on address", function () {
+			it("Should allow to read the account state of foreign apps based on address", function () {
 				const prog = `
 				txn Applications 2
 				app_params_get AppAddress
