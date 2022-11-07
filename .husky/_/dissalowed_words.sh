3762e7d13a0c94df7905283b54882837f2555c08
sczembor <43810037+sczembor@users.noreply.github.com>
2022-11-07 23:46:32 +0100

    test

--- .husky/_/dissalowed_words.sh
+++ .husky/_/dissalowed_words.sh
@@ -7,14 +7,22 @@ status=0
 
 echo "################################test"
 
-for file in $(git diff --name-only);
-do
-    echo "$file"
+files=$(git diff --name-only)
+echo "before loop"
+echo "$files"
+for file in $files; do
+  echo "am i even here?"
+  echo $file
+done
+
+# for file in $(git diff --name-only);
+# do
+#     echo "$file"
     # if [ "${file: -3}" != ".ts" ] && [ "${file: -3}" != ".js" ]; then continue; fi
     # for line in $(grep -in -E "\.only\("  "$file" | cut -f1 -d:);
     # do
     #     printf "%40s\n" "${COLOR_RED}ERROR:${COLOR_REST} Disallowed expression${COLOR_YELLOW} .only()${COLOR_REST} in file: $COLOR_MAGENTA${line}:${file}$COLOR_REST"
     #     status=1 
     # done
-done
+# done
 exit $status
