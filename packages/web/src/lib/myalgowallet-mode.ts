80d086738cf5280963d7f45c38715e08d53f3b8e
MEGHA <100185149+Megha-Dev-19@users.noreply.github.com>
2022-11-18 08:43:20 +0530

    fixes

--- packages/web/src/lib/myalgowallet-mode.ts
+++ packages/web/src/lib/myalgowallet-mode.ts
@@ -9,6 +9,7 @@ import type {
 	SignTransactionOptions,
 } from "@randlabs/myalgo-connect";
 import algosdk, { SignedTransaction, Transaction } from "algosdk";
+import { LogicSig } from "algosdk/dist/types/src/logicsig";
 
 import { mkTxParams } from "..";
 import {
